65df7885ddd5692783433cce453bacdd1ef6b133
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2022-12-01 06:46:18 +0100

    Update template

--- template.py
+++ template.py
@@ -8,52 +8,24 @@ DAY = datetime.datetime.today().day
 
 puzzle = Puzzle(year=YEAR, day=DAY)
 
-# Part a
-def a(input, debug=False):
-    if debug:
-        print(input)
-        breakpoint()
-    #input = np.fromstring(input, dtype=int, sep="\n"
-    #output = ...
-    if debug:
-        print(output)
-        breakpoint()
-    return output
 
-ANSWER_EXAMPLE = None
-try:
-    assert a(puzzle.example_data) == ANSWER_EXAMPLE
-except Exception as e:
-    print(e)
-    assert a(puzzle.example_data, debug=True) == ANSWER_EXAMPLE
-try:
-    answer = a(puzzle.input_data)
-except Exception as e:
-    print(e)
-    answer = a(puzzle.input_data, debug=True)
+# Part a
+def a(input):
+    ...
+
+ANSWER_EXAMPLE = ...
+assert a(puzzle.example_data) == ANSWER_EXAMPLE
+answer = a(puzzle.input_data)
+print("a:", answer)
 puzzle.answer_a = answer
 
-# Part b
-def b(input, debug=False):
-    if debug:
-        print(input)
-        breakpoint()
-    #input = np.fromstring(input, dtype=int, sep="\n"
-    #output = ...
-    if debug:
-        print(output)
-        breakpoint()
-    return output
 
-ANSWER_EXAMPLE = None
-try:
-    assert b(puzzle.example_data) == ANSWER_EXAMPLE
-except Exception as e:
-    print(e)
-    assert b(puzzle.example_data, debug=True) == ANSWER_EXAMPLE
-try:
-    answer = b(puzzle.input_data)
-except Exception as e:
-    print(e)
-    answer = b(puzzle.input_data, debug=True)
+# Part b
+def b(input):
+    ...
+
+ANSWER_EXAMPLE = ...
+assert b(puzzle.example_data) == ANSWER_EXAMPLE
+answer = b(puzzle.input_data)
+print("b:", answer)
 puzzle.answer_b = answer
