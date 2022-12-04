58bbecc4c58d9f279f07e1b2d8b4352019f9332a
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2022-12-04 06:21:36 +0100

    2022/04: Add solution

---
+++ 2022/04.py
@@ -0,0 +1,43 @@
+import datetime
+import re
+
+from aocd.models import Puzzle
+
+YEAR = 2022
+DAY = 4
+
+puzzle = Puzzle(year=YEAR, day=DAY)
+
+
+# Part a
+def a(data):
+    data = re.findall("(\d+)-(\d+),(\d+)-(\d+)", data)
+    data = [
+        set(range(int(r[0]), int(r[1]) + 1)).issubset(set(range(int(r[2]), int(r[3]) + 1)))
+        or set(range(int(r[2]), int(r[3]) + 1)).issubset(set(range(int(r[0]), int(r[1]) + 1)))
+        for r in data
+    ]
+    return sum(data)
+
+assert a(puzzle.example_data) == 2
+answer = a(puzzle.input_data)
+print("a:", answer)
+#puzzle.answer_a = answer
+assert answer == 571
+
+
+# Part b
+def b(data):
+    data = re.findall("(\d+)-(\d+),(\d+)-(\d+)", data)
+    data = [
+        set(range(int(r[0]), int(r[1]) + 1)).intersection(set(range(int(r[2]), int(r[3]) + 1)))
+        for r in data
+    ]
+    #breakpoint()
+    return sum([d !=  set() for d in data])
+
+assert b(puzzle.example_data) == 4
+answer = b(puzzle.input_data)
+print("b:", answer)
+#puzzle.answer_b = answer
+assert answer == 917
