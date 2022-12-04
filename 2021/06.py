8f29b9fc044145531e294ff06aa95b1fd0424580
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2022-12-04 14:45:07 +0100

    2021/06: Simplify solution

--- 2021/06.py
+++ 2021/06.py
@@ -12,16 +12,13 @@ def a(data, days):
     data = np.fromstring(data, dtype=np.uint8, sep=",")
     total = 0
     for fish in data:
-        old_fish = np.bincount([fish], minlength=7)
-        new_fish = np.zeros(9, dtype=int)
+        count = np.bincount([fish], minlength=9)
         for day in range(1, days + 1):
-            old_zero = old_fish[0]
-            old_fish = np.roll(old_fish, -1)
-            new_zero = new_fish[0]
-            new_fish = np.roll(new_fish, -1)
-            old_fish[6] = old_zero + new_zero
-            new_fish[8] = old_zero + new_zero
-        total += np.sum(old_fish) + np.sum(new_fish)
+            zero_count = count[0]
+            count = np.roll(count, -1)
+            count[6] += zero_count
+            count[8] = zero_count
+        total += np.sum(count)
     return total
 
 answer_example = a(puzzle.example_data, days=80)
