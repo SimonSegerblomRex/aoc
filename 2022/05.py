5642705bebe8f93c016f81f86560c4024689cf9c
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2022-12-05 17:29:50 +0100

    2022/05: Cleanup

--- 2022/05.py
+++ 2022/05.py
@@ -1,6 +1,5 @@
 import re
 
-import numpy as np
 from aocd.models import Puzzle
 
 YEAR = 2022
@@ -10,19 +9,25 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 
 
 # Part a
-def a(data):
-    ugly_boxes = re.findall(".{4}", data[:data.find("1")], re.DOTALL)
-    nbr_crates = int(re.findall("\d+\s+(\d+) \n", data)[0])
-    boxes = {}
+def _parse_crates(data):
+    ugly_crates = re.findall(r"(.{4})", data[: data.find("1")], re.DOTALL)
+    nbr_crates = int(re.findall(r"\d+\s+(\d+) \n", data)[0])
+    crates = {}
     for i in range(nbr_crates):
-        boxes[i + 1] = [e.strip(" \n[]") for e in ugly_boxes[i::nbr_crates] if e.strip(" \n[]")]
-    instructions = re.findall("move (\d+) from (\d+) to (\d+)", data)
+        crates[i + 1] = [e.strip(" \n[]") for e in ugly_crates[i::nbr_crates] if e.strip(" \n[]")]
+    return crates
+
+
+def a(data):
+    crates = _parse_crates(data)
+    instructions = re.findall(r"move (\d+) from (\d+) to (\d+)", data)
     for instruction in instructions:
-        nbr, fr, to = [int(e) for e in instruction]
+        nbr, fr, to = map(int, instruction)
         for _ in range(nbr):
-            box = boxes[fr].pop(0)
-            boxes[to].insert(0, box)
-    return "".join([stack[0] for stack in boxes.values()])
+            box = crates[fr].pop(0)
+            crates[to].insert(0, box)
+    return "".join(crate[0] for crate in crates.values())
+
 
 example_answer = a(puzzle.example_data)
 print(example_answer)
@@ -34,18 +39,15 @@ assert answer == "CNSZFDVLJ"
 
 # Part b
 def b(data):
-    ugly_boxes = re.findall(".{4}", data[:data.find("1")], re.DOTALL)
-    nbr_crates = int(re.findall("\d+\s+(\d+) \n", data)[0])
-    boxes = {}
-    for i in range(nbr_crates):
-        boxes[i + 1] = [e.strip(" \n[]") for e in ugly_boxes[i::nbr_crates] if e.strip(" \n[]")]
-    instructions = re.findall("move (\d+) from (\d+) to (\d+)", data)
+    crates = _parse_crates(data)
+    instructions = re.findall(r"move (\d+) from (\d+) to (\d+)", data)
     for instruction in instructions:
-        nbr, fr, to = [int(e) for e in instruction]
+        nbr, fr, to = map(int, instruction)
         for i in range(nbr):
-            box = boxes[fr].pop(0)
-            boxes[to].insert(i, box)
-    return "".join([stack[0] for stack in boxes.values()])
+            box = crates[fr].pop(0)
+            crates[to].insert(i, box)
+    return "".join(crate[0] for crate in crates.values())
+
 
 example_answer = b(puzzle.example_data)
 print(example_answer)
