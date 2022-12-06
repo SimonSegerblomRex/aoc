2021/11: Fix float precision problem and add convolve2d

2021/11.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.ndimage import uniform_filter
+from scipy.signal import convolve2d
 from aocd.models import Puzzle
 
 YEAR = 2021
@@ -21,9 +22,23 @@ def a(data):
             if not flash.any():
                 break
             grid[flash] = 0
-            hack = (
-                uniform_filter(flash.astype(float), size=3, mode="constant", cval=0) * 9
-            ).astype(np.uint8)
+            if 1:
+                hack = (
+                    uniform_filter(
+                        flash.astype(float),
+                        size=3,
+                        mode="constant",
+                        cval=0,
+                    )
+                    * 9 + 0.5
+                ).astype(np.uint8)
+            else:
+                hack = convolve2d(
+                    flash.astype(np.uint8),
+                    np.ones((3, 3), dtype=np.uint8),
+                    mode="same",
+                    boundary="fill",
+                )
             grid[grid > 0] += hack[grid > 0]
         flashes += (grid == 0).sum()
     return flashes
@@ -31,7 +46,7 @@ def a(data):
 
 example_answer = a(puzzle.example_data)
 print(example_answer)
-# assert example_answer == 1656  # Wrong in the example..?
+assert example_answer == 1656
 answer = a(puzzle.input_data)
 print("a:", answer)
 assert answer == 1700
@@ -50,9 +65,23 @@ def b(data):
             if not flash.any():
                 break
             grid[flash] = 0
-            hack = (
-                uniform_filter(flash.astype(float), size=3, mode="constant", cval=0) * 9
-            ).astype(np.uint8)
+            if 1:
+                hack = (
+                    uniform_filter(
+                        flash.astype(float),
+                        size=3,
+                        mode="constant",
+                        cval=0,
+                    )
+                    * 9 + 0.5
+                ).astype(np.uint8)
+            else:
+                hack = convolve2d(
+                    flash.astype(np.uint8),
+                    np.ones((3, 3), dtype=np.uint8),
+                    mode="same",
+                    boundary="fill",
+                )
             grid[grid > 0] += hack[grid > 0]
         if (grid == 0).all():
             return step
@@ -60,7 +89,7 @@ def b(data):
 
 example_answer = b(puzzle.example_data)
 print(example_answer)
-# assert example_answer == 195  # Wrong in the example..?
+assert example_answer == 195
 answer = b(puzzle.input_data)
 print("b:", answer)
 assert answer == 273