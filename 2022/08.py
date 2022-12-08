5e01f5ca98b8609c0f5712af0c5d1163c7f71f7a
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2022-12-08 11:08:52 +0100

    2022/08: Add messy code for part b

--- 2022/08.py
+++ 2022/08.py
@@ -99,49 +99,95 @@ def b(data):
             score[i,j] = up*down*left*right
     return score.max()
     """
-    max_score = 0
-    for i in range(height):
-        for j in range(width):
+    scores = np.zeros(grid.shape, dtype=int)
+    for i in range(1, height - 1):
+        for j in range(1, width -1):
+            right = grid[i, j:]
+            left = grid[i, :j+1][::-1]
+            up = grid[:i+1, j][::-1]
+            down = grid[i:,j]
+
+            def count(x):
+                if (len(x) <= 2) or (x[0] < x[1]):
+                    return 1
+                c = 1
+                for i in range(1, len(x) - 1):
+                    if (x[i] >= x[0]):
+                        break
+                    c += 1
+                return c
+                breakpoint()
+
+            scores[i,j] = count(up)*count(down)*count(left)*count(right)
+            #breakpoint()
+            """
+            right = 0
+            for x in range(j + 1, width):
+                if (grid[i, x] <= grid[i, j]) and (grid[i, x] >= grid[i, x - 1]):
+                    right +=1
+                else:
+                    if x < width - 1:
+                        right += 1
+                    break
+
+            left = 0
+            for x in range(j - 1, -1, -1):
+                if (grid[i, x] < grid[i, j]) and (grid[i,x] >= grid[i, x + 1]):
+                    left +=1
+                else:
+                    if x > 0:
+                        left += 1
+                    break
+
+            down = 0
+            for y in range(i + 1, height):
+                if (grid[y, j] <= grid[i, j]) and (grid[y,j] >= grid[y -1,j]):
+                    down +=1
+                else:
+                    if y < height -1:
+                        down += 1
+                    break
+
+            up = 0
+            for y in range(i - 1, -1, -1):
+                if (grid[y, j] <= grid[i, j]) and (grid[y,j] >= grid[y + 1,j]):
+                    up +=1
+                else:
+                    if y > 0:
+                        up += 1
+                    break
+
+            #scores[i,j] = min(up, 1) * min(down, 1) * min(left, 1) * min(right, 1)
+            scores[i,j] = up * down * left * right
+            """
+
+            """
             # right
             if j == width - 1:
                 right = 0
             else:
-                if (grid[i, :j] == grid[i, j +1]).all() and (grid[i, j + 1] < grid[i, j]):
-                    right = width - j - 1
-                    #breakpoint()
-                else:
-                    right = (grid[i, j + 1:].argmax() + 1) if (grid[i, j + 1] < grid[i, j]) else 1
+                right = (grid[i, j + 1:].argmax() + 1) if (grid[i, j + 1] <= grid[i, j]) else 1
             # left
             if j == 0:
                 left = 0
             else:
-                if (grid[i, :j] == grid[i, j -1]).all() and (grid[i, j - 1] < grid[i, j]):
-                    left = j
-                    #breakpoint()
-                else:
-                    left = (grid[i, :j][::-1].argmax() + 1) if (grid[i, j - 1] < grid[i, j]) else 1
+                left = (grid[i, :j].argmax()) if (grid[i, j - 1] <= grid[i, j]) else 1
             # up
             if i == 0:
                 up = 0
             else:
-                if (grid[:i, j] == grid[i - 1, j]).all() and (grid[i - 1, j] < grid[i, j]):
-                    up = i
-                    #breakpoint()
-                else:
-                    up = (grid[:i, j][::-1].argmax() + 1) if (grid[i - 1, j] < grid[i, j]) else 1
+                up = (grid[:i, j][::-1].argmax() + 1) if (grid[i - 1, j] <= grid[i, j]) else 1
             # down
             if i == height - 1:
                 down = 0
             else:
-                if (grid[i, :j] == grid[i + 1, j]).all() and (grid[i + 1, j] < grid[i, j]):
-                    down = height - i - 1
-                    #breakpoint()
-                else:
-                    down = (grid[i + 1:, j].argmax() + 1) if (grid[i + 1, j] < grid[i, j]) else 1
-            print(up, left, down, right, "a", i, j, "b", up*down*left*right)
-            max_score = max(max_score, up * down * left * right)
+                down = (grid[i + 1:, j].argmax() + 1) if (grid[i + 1, j] <= grid[i, j]) else 1
+            """
+            #max_score = max(max_score, up * down * left * right)
 
-    return max_score
+    #breakpoint()
+    print(scores)
+    return scores.max()#max_score
 
 example_answer = b(puzzle.example_data)
 print(example_answer)
