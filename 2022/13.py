c892b9ddf0c2345ce34fe76597a6d1e755fb8eb6
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2022-12-13 11:38:35 +0100

    2022/13: Add solution for part b

--- 2022/13.py
+++ 2022/13.py
@@ -1,4 +1,5 @@
 import datetime
+import functools
 import re
 
 import numpy as np
@@ -12,40 +13,40 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 
 def check(l0, l1):
     if not l0 and not l1:
-        return None
+        return 0
     if not l0:
-        return True
+        return 1
     if not l1:
-        return False
+        return -1
     for v0, v1 in zip(l0, l1):
         if isinstance(v0, int) and isinstance(v1, int):
             if v0 < v1:
-                return True
+                return 1
             if v1 < v0:
-                return False
+                return -1
         elif isinstance(v0, list) and isinstance(v1, list):
             c = check(v0, v1)
             #return c
-            if c is not None:
+            if c != 0:
                 return c
         elif isinstance(v0, list) and isinstance(v1, int):
             c = check(v0, [v1])
             #return c
-            if c is not None:
+            if c != 0:
                 return c
         elif isinstance(v0, int) and isinstance(v1, list):
             c = check([v0], v1)
             #return c
-            if c is not None:
+            if c != 0:
                 return c
         else:
             print("Shouldn't end up here!")
             breakpoint()
     if len(l1) < len(l0):
-        return False
+        return -1
     if len(l0) < len(l1):
-        return True
-    return None
+        return 1
+    return 0
 
 # Part a
 def a(data):
@@ -54,8 +55,7 @@ def a(data):
         pairs.append([eval(line) for line in lines.split("\n")])
     s = 0
     for i, p in enumerate(pairs):
-        c = check(p[0], p[1])
-        if c or c is None:
+        if check(p[0], p[1]) >= 0:
             print("r:", i + 1)
             s += i + 1
     return s
@@ -70,11 +70,22 @@ assert answer == 6428
 
 # Part b
 def b(data):
-    exit()
+    packets = [eval(line) for line in data.replace("\n\n", "\n").splitlines()]
+    packets.append([[2]])
+    packets.append([[6]])
+    packets = sorted(packets, key=functools.cmp_to_key(check), reverse=True)
+    i0 = None
+    i1 = None
+    for i, p in enumerate(packets):
+        if p == [[2]]:
+            i0 = i + 1
+        elif p == [[6]]:
+            i1 = i + 1
+    return i0 * i1
 
 example_answer = b(puzzle.example_data)
 print(example_answer)
-assert example_answer == ...
+assert example_answer == 140
 answer = b(puzzle.input_data)
 print("b:", answer)
 puzzle.answer_b = answer
