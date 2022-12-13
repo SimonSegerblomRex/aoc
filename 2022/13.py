13ec28dae46d3d22f1bd49a372e9a14a41fadd6d
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2022-12-13 11:19:45 +0100

    2022/13: Working solution for part a

--- 2022/13.py
+++ 2022/13.py
@@ -11,33 +11,41 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 
 
 def check(l0, l1):
-    #if len(l1) < len(l0):
-    #    return False
-    #l1 = l1[:len(l0)]
-    """
+    if not l0 and not l1:
+        return None
     if not l0:
         return True
     if not l1:
         return False
-    """
     for v0, v1 in zip(l0, l1):
         if isinstance(v0, int) and isinstance(v1, int):
             if v0 < v1:
                 return True
-            elif v1 < v0:
+            if v1 < v0:
                 return False
         elif isinstance(v0, list) and isinstance(v1, list):
-            return check(v0, v1)
+            c = check(v0, v1)
+            #return c
+            if c is not None:
+                return c
         elif isinstance(v0, list) and isinstance(v1, int):
-            return check(v0, [v1])
+            c = check(v0, [v1])
+            #return c
+            if c is not None:
+                return c
         elif isinstance(v0, int) and isinstance(v1, list):
-            return check([v0], v1)
+            c = check([v0], v1)
+            #return c
+            if c is not None:
+                return c
         else:
             print("Shouldn't end up here!")
             breakpoint()
     if len(l1) < len(l0):
         return False
-    return True
+    if len(l0) < len(l1):
+        return True
+    return None
 
 # Part a
 def a(data):
@@ -46,8 +54,9 @@ def a(data):
         pairs.append([eval(line) for line in lines.split("\n")])
     s = 0
     for i, p in enumerate(pairs):
-        if check(p[0], p[1]):
-            print(i + 1)
+        c = check(p[0], p[1])
+        if c or c is None:
+            print("r:", i + 1)
             s += i + 1
     return s
 
@@ -56,7 +65,7 @@ print(example_answer)
 assert example_answer == 13
 answer = a(puzzle.input_data)
 print("a:", answer)
-puzzle.answer_a = answer
+assert answer == 6428
 
 
 # Part b
