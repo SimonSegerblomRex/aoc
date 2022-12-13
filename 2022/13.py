4f928f55d21a09f943a50fe773dd45d0799745a7
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2022-12-13 18:54:08 +0100

    2022/13: Cleanup

--- 2022/13.py
+++ 2022/13.py
@@ -1,7 +1,6 @@
 import ast
 import functools
 
-import numpy as np
 from aocd.models import Puzzle
 
 YEAR = 2022
@@ -11,33 +10,19 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 
 
 def check(l0, l1):
-    if not l0 and not l1:
-        return 0
-    if not l0:
-        return -1
-    if not l1:
-        return 1
     for v0, v1 in zip(l0, l1):
         if isinstance(v0, int) and isinstance(v1, int):
             if v0 < v1:
                 return -1
             if v1 < v0:
                 return 1
-        elif isinstance(v0, list) and isinstance(v1, list):
-            c = check(v0, v1)
-            if c != 0:
-                return c
-        elif isinstance(v0, list) and isinstance(v1, int):
-            c = check(v0, [v1])
-            if c != 0:
-                return c
-        elif isinstance(v0, int) and isinstance(v1, list):
-            c = check([v0], v1)
-            if c != 0:
-                return c
         else:
-            print("Shouldn't end up here!")
-            breakpoint()
+            if not isinstance(v0, list):
+                v0 = [v0]
+            if not isinstance(v1, list):
+                v1 = [v1]
+            if (ret := check(v0, v1)) != 0:
+                return ret
     if len(l0) < len(l1):
         return -1
     if len(l1) < len(l0):
@@ -47,14 +32,8 @@ def check(l0, l1):
 
 # Part a
 def a(data):
-    pairs = []
-    for lines in data.split("\n\n"):
-        pairs.append([ast.literal_eval(line) for line in lines.split("\n")])
-    s = 0
-    for i, p in enumerate(pairs):
-        if check(p[0], p[1]) <= 0:
-            s += i + 1
-    return s
+    pairs = ([ast.literal_eval(line) for line in lines.split("\n")] for lines in data.split("\n\n"))
+    return sum(i + 1 for i, p in enumerate(pairs) if check(*p) <= 0)
 
 
 example_answer = a(puzzle.example_data)
@@ -68,8 +47,7 @@ assert answer == 6428
 # Part b
 def b(data):
     packets = [ast.literal_eval(line) for line in data.replace("\n\n", "\n").splitlines()]
-    packets.append([[2]])
-    packets.append([[6]])
+    packets.extend([[[2]], [[6]]])
     packets = sorted(packets, key=functools.cmp_to_key(check))
     return (packets.index([[2]]) + 1) * (packets.index([[6]]) + 1)
 
