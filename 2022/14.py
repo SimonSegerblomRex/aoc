d98d0a3ef6ea684ed4b0f97aba15f61a2442950f
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2022-12-14 06:56:14 +0100

    2022/14: Add solution to part b

--- 2022/14.py
+++ 2022/14.py
@@ -52,21 +52,62 @@ def a(data):
                 break
     return counter - 1
 
+"""
 example_answer = a(puzzle.example_data)
 print(example_answer)
 assert example_answer == 24
 answer = a(puzzle.input_data)
 print("a:", answer)
 puzzle.answer_a = answer
-
+"""
 
 # Part b
 def b(data):
-    exit()
+    grid = np.zeros((1000, 1000), dtype=int)
+    for line in data.splitlines():
+        coords = line.split(" -> ")
+        coords = [(int(coord.split(",")[1]), int(coord.split(",")[0])) for coord in coords]
+        for i in range(len(coords) - 1):
+            i0, j0 = coords[i]
+            i1, j1 = coords[i + 1]
+            if j0 != j1:
+                assert i0 == i1
+                grid[i0, min(j0, j1):max(j0, j1) + 1] = 1
+            else:
+                assert j0 == j1
+                grid[min(i0, i1):max(i0, i1) + 1, j0] = 1
+    #grid[0, 500] = 2
+    floor = np.argmax(grid==1, axis=0).max() + 2
+    grid[floor, :] = 1
+    grid = grid[:floor + 1, :]
+    print(grid[:12,494:504])
+    breakpoint()
+    game_over = False
+    counter = 0
+    while not game_over:
+        counter += 1
+        i0 = 0
+        j0 = 500
+        #print(i0, j0, counter)
+        while True:
+            if np.argmax(grid[i0:, j0]) == 0:
+                game_over = True
+                break
+            i0 = i0 + np.argmax(grid[i0:, j0] != 0) - 1
+            if grid[i0 + 1, j0 - 1] <= 0:
+                i0 += 1
+                j0 -= 1
+            elif grid[i0 + 1, j0 + 1] <= 0:
+                i0 += 1
+                j0 += 1
+            else:
+                grid[i0, j0] = 2
+                break
+    return counter - 1
 
 example_answer = b(puzzle.example_data)
 print(example_answer)
-assert example_answer == ...
+assert example_answer == 93
 answer = b(puzzle.input_data)
 print("b:", answer)
 puzzle.answer_b = answer
