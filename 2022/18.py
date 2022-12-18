b9424b79fa416a0a832b6b07eb88e569ec2bb329
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2022-12-18 08:03:33 +0100

    2022/18: Add solution for part b

--- 2022/18.py
+++ 2022/18.py
@@ -1,18 +1,27 @@
-import datetime
-import re
-
 import numpy as np
 from aocd.models import Puzzle
+from scipy.ndimage import binary_fill_holes
 
-YEAR = datetime.datetime.today().year
-DAY = datetime.datetime.today().day
+
+YEAR = 2022
+DAY = 18
 
 puzzle = Puzzle(year=YEAR, day=DAY)
 
 
 # Part a
+def calc_surface_area(grid):
+    return (
+        np.sum(np.diff(grid, axis=0) > 0)
+        + np.sum(np.diff(grid[::-1, :, :], axis=0) > 0)
+        + np.sum(np.diff(grid, axis=1) > 0)
+        + np.sum(np.diff(grid[:, ::-1, :], axis=1) > 0)
+        + np.sum(np.diff(grid, axis=2) > 0)
+        + np.sum(np.diff(grid[:, :, ::-1], axis=2) > 0)
+    )
+
 def a(data):
-    data =  [np.fromstring(line, dtype=int, sep=",") for line in data.splitlines()]
+    data = [np.fromstring(line, dtype=int, sep=",") for line in data.splitlines()]
     data = np.vstack(data)
     xx = data[:, 1]
     yy = data[:, 0]
@@ -24,7 +33,7 @@ def a(data):
     for x, y, z in zip(xx, yy, zz):
         grid[x, y, z] = 1
     grid = np.pad(grid, 1)
-    return np.sum(np.diff(grid, axis=0) > 0) + np.sum(np.diff(grid[::-1, :, :], axis=0) > 0) + np.sum(np.diff(grid, axis=1) > 0) + np.sum(np.diff(grid[:, ::-1, :], axis=1) > 0) + np.sum(np.diff(grid, axis=2) > 0) + np.sum(np.diff(grid[:, :, ::-1], axis=2) > 0)
+    return calc_surface_area(grid)
 
 
 example_answer = a(puzzle.example_data)
@@ -37,11 +46,26 @@ assert answer == 3326
 
 # Part b
 def b(data):
-    exit()
+    data = [np.fromstring(line, dtype=int, sep=",") for line in data.splitlines()]
+    data = np.vstack(data)
+    xx = data[:, 1]
+    yy = data[:, 0]
+    zz = data[:, 2]
+    width = np.max(xx) + 1
+    height = np.max(yy) + 1
+    depth = np.max(zz) + 1
+    grid = np.zeros((width, height, depth), dtype=int)
+    for x, y, z in zip(xx, yy, zz):
+        grid[x, y, z] = 1
+    grid = np.pad(grid, 1)
+    filled = binary_fill_holes(grid)
+    trapped = filled - grid
+    return calc_surface_area(grid) - calc_surface_area(trapped)
+
 
 example_answer = b(puzzle.example_data)
 print(example_answer)
 assert example_answer == 58
 answer = b(puzzle.input_data)
 print("b:", answer)
-puzzle.answer_b = answer
+assert answer == 1996
