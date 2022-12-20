21ae6c6c5255cf04021ef20d91a1ec4b472d21d7
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2022-12-20 08:25:34 +0100

    2022/20: Add solution for part b

--- 2022/20.py
+++ 2022/20.py
@@ -8,39 +8,38 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 
 
 # Part a
-def mix(data):
+def mix(data, order):
     data = np.array(data).tolist()
-    moved = np.full(len(data), False)
-    moved = moved.tolist()
     old_pos = 0
     l = len(data)
     for i in range(l):
-        old_pos = np.argmin(moved)
-        v = data[old_pos]
+        old_pos = order.index(i)
+        v = data.pop(old_pos)
+        o = order.pop(old_pos)
         new_pos = (old_pos + v) % (l - 1)
         if new_pos == 0:
             new_pos = l - 1
-        moved.pop(old_pos)
-        data.pop(old_pos)
         data.insert(new_pos, v)
-        moved.insert(new_pos, True)
+        order.insert(new_pos, o)
         if 0:
             print(v, old_pos, new_pos)
             print(data)
             breakpoint()
-    i0 = data.index(0)
-    return data[(i0 + 1000) % l] + data[(i0 + 2000) % l] + data[(i0 + 3000) % l]
+    return data, order
 
 
 def a(data):
     data = np.fromstring(data, dtype=int, sep="\n")
-    return mix(data)
+    order = list(range(len(data)))
+    data, _ = mix(data, order)
+    i0 = data.index(0)
+    l = len(data)
+    return data[(i0 + 1000) % l] + data[(i0 + 2000) % l] + data[(i0 + 3000) % l]
 
 
 example_answer = a(puzzle.example_data)
 print(example_answer)
 assert example_answer == 3
-breakpoint()
 answer = a(puzzle.input_data)
 print("a:", answer)
 puzzle.answer_a = answer
@@ -48,12 +47,19 @@ puzzle.answer_a = answer
 
 # Part b
 def b(data):
-    exit()
+    data = np.fromstring(data, dtype=int, sep="\n")
+    data *= 811589153
+    order = list(range(len(data)))
+    for _ in range(10):
+        data, order = mix(data, order)
+    i0 = data.index(0)
+    l = len(data)
+    return data[(i0 + 1000) % l] + data[(i0 + 2000) % l] + data[(i0 + 3000) % l]
 
 
 example_answer = b(puzzle.example_data)
 print(example_answer)
-assert example_answer == 3
+assert example_answer == 1623178306
 answer = b(puzzle.input_data)
 print("b:", answer)
 puzzle.answer_b = answer
