15083d659aa981c8121aed36b201e594a3875be4
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2022-12-25 22:31:18 +0100

    2022/19: First draft of brute-force solution

---
+++ 2022/19.py
@@ -0,0 +1,106 @@
+import re
+from dataclasses import dataclass
+from functools import partial
+
+import numpy as np
+from aocd.models import Puzzle
+
+YEAR = 2022
+DAY = 19
+
+puzzle = Puzzle(year=YEAR, day=DAY)
+
+EXAMPLE_DATA = """Blueprint 1:
+  Each ore robot costs 4 ore.
+  Each clay robot costs 2 ore.
+  Each obsidian robot costs 3 ore and 14 clay.
+  Each geode robot costs 2 ore and 7 obsidian.
+
+Blueprint 2:
+  Each ore robot costs 2 ore.
+  Each clay robot costs 3 ore.
+  Each obsidian robot costs 3 ore and 8 clay.
+  Each geode robot costs 3 ore and 12 obsidian."""
+
+PATTERN = r"Blueprint (\d+):\s+Each ore robot costs (\d+) ore.\s+Each clay robot costs (\d+) ore.\s+Each obsidian robot costs (\d+) ore and (\d+) clay.\s+Each geode robot costs (\d+) ore and (\d+) obsidian."
+
+Cost = partial(np.array, dtype=int)
+
+@dataclass
+class Blueprint:
+    nbr: int
+    costs: list
+
+
+# Part a
+def a(data):
+    data = re.findall(PATTERN, data)
+    blueprints = [
+        Blueprint(
+            nbr=e[0],
+            costs=np.array([
+                Cost((e[1], 0, 0, 0)),
+                Cost((e[2],0 ,0, 0)),
+                Cost((e[3], e[4], 0, 0)),
+                Cost((e[5], 0, e[6], 0)),
+            ]),
+        )
+        for e in data
+    ]
+    blueprint = blueprints[0]  # FIXME: Check all blueprints
+
+    # brute-force
+    debug_options = [1, 1, 1, 2, 1, 2, 3, 3]
+    def find_paths(blueprint, robots, stock, time_remaining):
+        next_robot_options = [i for i, c in enumerate(blueprint.costs) if robots[np.flatnonzero(c)].all()]
+        #for next_robot in [] if not debug_options else [debug_options.pop(0)]:
+        for next_robot in next_robot_options[::-1]:
+            next_robots = robots.copy()
+            if ((blueprint.costs[next_robot] - stock) <= 0).all():
+                time_required = 1
+            else:
+                time_required = int(np.ceil(np.nanmax((blueprint.costs[next_robot] - stock) / robots))) + 1
+            next_time_remaining = time_remaining - time_required
+            if next_time_remaining < 0:
+                continue
+            next_stock = stock + time_required * robots - blueprint.costs[next_robot]
+            next_robots[next_robot] += 1
+            if 0:
+                print("Current:", "robots:", robots, "stock:", stock, "time_remaining:", time_remaining)
+                print("Next:", "robots:", next_robots, "stock:", next_stock, "time_remaining:", next_time_remaining)
+            yield from find_paths(
+                blueprint, next_robots, next_stock, next_time_remaining
+            )
+        stock += robots * time_remaining
+        yield stock, robots
+
+    robots = np.array((1, 0, 0, 0))
+    stock = np.array((0, 0, 0, 0))
+    tmp = find_paths(blueprint, robots, stock, 24)
+    m = 0
+    for s, r in tmp:
+        if s[-1] > m:
+            m = s[-1]
+            print(s, r)
+    #hmm = max(tmp)
+    breakpoint()
+
+
+example_answer = a(EXAMPLE_DATA)
+print(example_answer)
+assert example_answer == 33
+answer = a(puzzle.input_data)
+print("a:", answer)
+puzzle.answer_a = answer
+
+
+# Part b
+def b(data):
+    exit()
+
+example_answer = b(EXAMPLE_DATA)
+print(example_answer)
+assert example_answer == ...
+answer = b(puzzle.input_data)
+print("b:", answer)
+puzzle.answer_b = answer
