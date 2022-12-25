f2d724c89c875aded0c58584a612037e5b7d2b44
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2022-12-26 00:43:26 +0100

    2022/19: Add solution for part a
    Got the hint about using the triangular number sequence for
    estimating an upper limit of the possible number of geodes
    to produce.

--- 2022/19.py
+++ 2022/19.py
@@ -37,7 +37,7 @@ def a(data):
     data = re.findall(PATTERN, data)
     blueprints = [
         Blueprint(
-            nbr=e[0],
+            nbr=int(e[0]),
             costs=np.array([
                 Cost((e[1], 0, 0, 0)),
                 Cost((e[2],0 ,0, 0)),
@@ -47,43 +47,46 @@ def a(data):
         )
         for e in data
     ]
-    blueprint = blueprints[0]  # FIXME: Check all blueprints
 
-    # brute-force
-    debug_options = [1, 1, 1, 2, 1, 2, 3, 3]
-    def find_paths(blueprint, robots, stock, time_remaining):
-        next_robot_options = [i for i, c in enumerate(blueprint.costs) if robots[np.flatnonzero(c)].all()]
-        #for next_robot in [] if not debug_options else [debug_options.pop(0)]:
-        for next_robot in next_robot_options[::-1]:
-            next_robots = robots.copy()
-            if ((blueprint.costs[next_robot] - stock) <= 0).all():
-                time_required = 1
-            else:
-                time_required = int(np.ceil(np.nanmax((blueprint.costs[next_robot] - stock) / robots))) + 1
-            next_time_remaining = time_remaining - time_required
-            if next_time_remaining < 0:
-                continue
-            next_stock = stock + time_required * robots - blueprint.costs[next_robot]
-            next_robots[next_robot] += 1
-            if 0:
-                print("Current:", "robots:", robots, "stock:", stock, "time_remaining:", time_remaining)
-                print("Next:", "robots:", next_robots, "stock:", next_stock, "time_remaining:", next_time_remaining)
-            yield from find_paths(
-                blueprint, next_robots, next_stock, next_time_remaining
-            )
-        stock += robots * time_remaining
-        yield stock, robots
+    s = 0
+    for blueprint in blueprints:
+        best_so_far = [0]
+        debug_options = [1, 1, 1, 2, 1, 2, 3, 3]
+        def find_paths(blueprint, robots, stock, time_remaining):
+            next_robot_options = [i for i, c in enumerate(blueprint.costs) if robots[np.flatnonzero(c)].all()]
+            already_enough_robots = np.flatnonzero((robots >= blueprint.costs.max(axis=0))[:3])
+            next_robot_options = list(set(next_robot_options) - set(already_enough_robots))
+            if robots[-1] * time_remaining + time_remaining * (time_remaining + 1) / 2 + stock[-1] < best_so_far[0]:
+                next_robot_options = []
+            #for next_robot in [] if not debug_options else [debug_options.pop(0)]:
+            for next_robot in next_robot_options[::-1]:
+                if ((blueprint.costs[next_robot] - stock) <= 0).all():
+                    time_required = 1
+                else:
+                    time_required = int(np.ceil(np.nanmax((blueprint.costs[next_robot] - stock) / robots))) + 1
+                    if time_required <= 0:
+                        raise ValueError
+                    #breakpoint()
+                next_time_remaining = time_remaining - time_required
+                if next_time_remaining < 0:
+                    continue
+                next_stock = stock + time_required * robots - blueprint.costs[next_robot]
+                next_robots = robots.copy()
+                next_robots[next_robot] += 1
+                if 0:
+                    print("Current:", "robots:", robots, "stock:", stock, "time_remaining:", time_remaining)
+                    print("Next:", "robots:", next_robots, "stock:", next_stock, "time_remaining:", next_time_remaining)
+                yield from find_paths(
+                    blueprint, next_robots, next_stock, next_time_remaining
+                )
+            stock += robots * time_remaining
+            best_so_far[0] = max(best_so_far[0], stock[-1])
+            yield stock[-1]
 
-    robots = np.array((1, 0, 0, 0))
-    stock = np.array((0, 0, 0, 0))
-    tmp = find_paths(blueprint, robots, stock, 24)
-    m = 0
-    for s, r in tmp:
-        if s[-1] > m:
-            m = s[-1]
-            print(s, r)
-    #hmm = max(tmp)
-    breakpoint()
+        robots = np.array((1, 0, 0, 0))
+        stock = np.array((0, 0, 0, 0))
+        s += blueprint.nbr * max(find_paths(blueprint, robots, stock, 24))
+    return s
 
 
 example_answer = a(EXAMPLE_DATA)
