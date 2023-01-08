d8c59a6a7a2d052c35b91d8d21abebca493376bb
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-01-08 09:21:58 +0100

    2021/16: Add solution for part b

--- 2021/16.py
+++ 2021/16.py
@@ -1,5 +1,3 @@
-import re
-
 import numpy as np
 from aocd.models import Puzzle
 
@@ -19,7 +17,7 @@ def unpack_literal(bits):
         if chunk[0] == 0:
             break
     itemsize = np.min_scalar_type((1 << len(literal)) - 1).itemsize
-    literal = np.pad(literal, (0, itemsize * 8 - len(literal)))
+    literal = np.pad(literal, (itemsize * 8 - len(literal), 0))
     literal = np.packbits(literal[::-1], bitorder="little").view(f"<u{itemsize}")[0]
     return literal, curr_loc
 
@@ -34,21 +32,23 @@ def parse_packet(bits):
     p_type = np.packbits(p_type[::-1], bitorder="little")[0]
     curr_loc += 6
     if p_type == 4:
-        _, nbr_bits = unpack_literal(bits[curr_loc:])
+        value, nbr_bits = unpack_literal(bits[curr_loc:])
         curr_loc += nbr_bits
     else:
         l_type = bits[curr_loc]
         curr_loc += 1
+        sub_packet_values = []
         if l_type == 0:
             sub_packet_bits = bits[curr_loc : curr_loc + 15]
             sub_packet_bits = np.packbits(sub_packet_bits[::-1], bitorder="little").view("<u2")[0]
             curr_loc += 15
             n = 0
             while n < sub_packet_bits:
-                e, nbr_bits = parse_packet(bits[curr_loc:])
+                e, nbr_bits, v = parse_packet(bits[curr_loc:])
                 s += e
                 n += nbr_bits
                 curr_loc += nbr_bits
+                sub_packet_values.append(v)
             assert n == sub_packet_bits
         else:
             nbr_sub_packets = bits[curr_loc : curr_loc + 11]
@@ -57,17 +57,31 @@ def parse_packet(bits):
             ).view("<u2")[0]
             curr_loc += 11
             for _ in range(nbr_sub_packets):
-                e, nbr_bits = parse_packet(bits[curr_loc:])
+                e, nbr_bits, v = parse_packet(bits[curr_loc:])
                 s += e
                 curr_loc += nbr_bits
-    return s, curr_loc
+                sub_packet_values.append(v)
+        ops = {
+            0: np.sum,
+            1: np.prod,
+            2: np.min,
+            3: np.max,
+            5: np.greater,
+            6: np.less,
+            7: np.equal,
+        }
+        if p_type < 4:
+            value = int(ops[p_type](sub_packet_values))
+        else:
+            value = int(ops[p_type](*sub_packet_values))
+    return s, curr_loc, value
 
 
 def a(data):
     bits = np.unpackbits(np.frombuffer(bytes.fromhex(data), dtype=np.uint8))
     s = 0
     while len(bits) > 7:
-        e, nbr_bits = parse_packet(bits)
+        e, nbr_bits, _ = parse_packet(bits)
         bits = bits[nbr_bits:]
         s += e
     return s
@@ -88,12 +102,18 @@ assert answer == 920
 
 # Part b
 def b(data):
-    exit()
+    bits = np.unpackbits(np.frombuffer(bytes.fromhex(data), dtype=np.uint8))
+    return parse_packet(bits)[-1]
 
 
-example_answer = b(puzzle.example_data)
-print(example_answer)
-assert example_answer == ...
+assert b("C200B40A82") == 3
+assert b("04005AC33890") == 54
+assert b("880086C3E88112") == 7
+assert b("CE00C43D881120") == 9
+assert b("D8005AC2A8F0") == 1
+assert b("F600BC2D8F") == 0
+assert b("9C005AC2F8F0") == 0
+assert b("9C0141080250320F1802104A08") == 1
 answer = b(puzzle.input_data)
 print("b:", answer)
-puzzle.answer_b = answer
+assert answer == 10185143721112
