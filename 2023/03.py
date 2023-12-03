717a8cbe6167b6038d16e5d890287d4ab054695d
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-12-03 10:24:31 +0100

    2023/03: Add working (ugly messy) solution for part a

--- 2023/03.py
+++ 2023/03.py
@@ -13,16 +13,17 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 
 
 # Part a
-def a(data):
+def a_old_broken(data):
     s_good = 0
     s_bad = 0
+    hmm = []
     grid = np.vstack(
         [np.frombuffer(n.encode(), dtype=np.uint8) - ord("0") for n in data.split("\n")]
     )
     symbols = np.logical_and(grid != 254, grid > 9)
     kernel = np.array([
         [1, 1, 1],
-        [1, 0, 1],
+        [1, 1, 1],
         [1, 1, 1],
     ])
     hack = convolve2d(
@@ -46,6 +47,7 @@ def a(data):
         tt[~good] = -1
         tmp = "".join(str(c) for c in tt)
         print([int(e) for e in tmp.split("-1") if e])
+        hmm.extend([int(e) for e in tmp.split("-1") if e])
         s_good += sum([int(e) for e in tmp.split("-1") if e])
     bad_digits = np.logical_and(digits, ~good_digits)
     for row, good in zip(grid.copy(), bad_digits):
@@ -57,8 +59,46 @@ def a(data):
     s_total = sum(int(n) for n in re.findall(r"(\d+)", data))
     print(s_total, s_good, s_bad)
     assert s_total == s_good + s_bad
+    for iy, ix in np.ndindex(grid.shape):
+        if iy == 0:
+            continue
+        try:
+            if (0<= grid[iy, ix] <= 9) and ((0<= grid[iy- 1, ix] <= 9) or (0<= grid[iy + 1, ix] <= 9)):
+                breakpoint()
+        except IndexError:
+            continue
+    #assert len(np.unique(hmm)) == len(hmm)
+    #return sum(np.unique(hmm))
+    #breakpoint()
     return s_good
 
+def a(data):
+    grid = np.vstack(
+        [np.frombuffer(n.encode(), dtype=np.uint8) - ord("0") for n in data.split("\n")]
+    )
+    height, width = grid.shape
+    grid = np.pad(grid, 1, constant_values = 254)
+    s_bad = 0
+    for y in range(1, height + 1):
+        bad_numbers = []
+        candidate = []
+        looking = False
+        for x in range(0, width + 1):
+            if (grid[y, x] == 254) and (grid[y - 1, x] == 254) and (grid[y + 1, x] == 254):
+                if candidate:
+                    s_bad += int("".join(str(n) for n in candidate))
+                looking = True
+                candidate = []
+            elif looking and (grid[y, x] < 10) and (grid[y - 1, x] == 254) and (grid[y + 1, x] == 254):
+                candidate.append(grid[y, x])
+            else:
+                candidate = []
+                looking = False
+        if looking and candidate:
+            s_bad += int("".join(str(n) for n in candidate))
+    s_total = sum(int(n) for n in re.findall(r"(\d+)", data))
+    return s_total - s_bad
+    breakpoint()
 
 for example in puzzle.examples:
     if example.answer_a:
@@ -66,22 +106,24 @@ for example in puzzle.examples:
         print(f"Example answer: {example_answer} (expecting: {example.answer_a})")
         assert str(example_answer) == example.answer_a
 
-example2 = """407..114..
-...*......
-..35..633.
-.....1#..*
-617*.....2
-.....+.58.
-..592...22
-..9...755-
-...$2*....
-.664.598.."""
-print(a(example2), 4361 - 60 + 9 + 1 + 2 + 2 + 22, "AAAAAAAAAAA")
-assert a(example2) == 4361 - 60 + 9 + 1 + 2 + 2 + 22
-breakpoint()
+if 0:
+    example2 = """407..114..
+    ...*......
+    ..35..633.
+    .....1#..*
+    617*.....2
+    .....+.58.
+    ..592...22
+    ..9...755-
+    ...$2*....
+    .664.598.."""
+    print(a(example2), 4361 - 60 + 9 + 1 + 2 + 2 + 22, "AAAAAAAAAAA")
+    assert a(example2) == 4361 - 60 + 9 + 1 + 2 + 2 + 22
+    breakpoint()
 
 answer = a(puzzle.input_data)
 print("a:", answer)
+print("old:", a_old_broken(puzzle.input_data), " vs ", answer)
 assert answer > 457353
 puzzle.answer_a = answer
 
