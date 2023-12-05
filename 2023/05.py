7f3f4c6a07cd88a68afae867a04b4e216ebde40c
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-12-05 11:39:28 +0100

    2023/05: Kladd...

--- 2023/05.py
+++ 2023/05.py
@@ -39,13 +39,42 @@ for example in puzzle.examples:
         assert str(example_answer) == example.answer_a
 answer = a(puzzle.input_data)
 print("a:", answer)
-puzzle.answer_a = answer
-
+assert answer == 806029445
 
 # Part b
 def b(data):
-    #print(data)
-    print("BBB")
+    lines = iter(data.splitlines())
+    seeds = np.fromstring(next(lines).split(":")[1], dtype=int, sep=" ")
+    big_map = {}
+    for match in re.finditer(r"(?P<from>\w+)-to-(?P<to>\w+) map:\n(?P<maps>[\d\s\n]+)", data):
+        maps = np.fromstring(match["maps"], dtype=int, sep=" ")
+        maps.shape = (-1, 3)
+        big_map[f"{match['from']}-{match['to']}"] = {
+            "source_ranges": [range(r[1], r[1] + r[2]) for r in maps],
+            "destination_ranges": [range(r[0], r[0] + r[2]) for r in maps],
+        }
+    seeds.shape = (-1, 2)
+    seed_ranges = [range(r[0], r[0] + r[1]) for r in seeds]
+    input_ranges = seed_ranges.copy()
+    for step_name, step_ranges in big_map.items():
+        print(step_name)
+        print(input_ranges)
+        mapped = []
+        intersection_ranges = []
+        for input_range in input_ranges:
+            for source_range, destination_range in zip(step_ranges["source_ranges"], step_ranges["destination_ranges"]):
+                intersection_range = range(max(input_range.start, source_range.start), min(input_range.stop, source_range.stop))
+                if intersection_range.stop > intersection_range.start:
+                    intersection_ranges.append(intersection_range)
+                    mapped.append(range(destination_range.start, intersection_range.stop - intersection_range.start))
+            # find ranges in input_range not mapped...
+            for intersection_range in sorted(intersection_ranges):
+                breakpoint()
+        input_ranges = mapped.copy()
+        #input_ranges.extend(not_mapped)
+    tmp2 = min(r.start for r in input_ranges if r.stop > r.start)
+    breakpoint()
+    return ll
     breakpoint()
 
 
