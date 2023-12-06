f4054ac65c63b97d2778e82d392489d6a124af1e
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-12-06 06:40:22 +0100

    2023/06: Minor cleanup

--- 2023/06.py
+++ 2023/06.py
@@ -1,53 +1,34 @@
-import datetime
-import re
-
 import numpy as np
 from aocd.models import Puzzle
 
-YEAR = datetime.datetime.today().year
-DAY = datetime.datetime.today().day
+YEAR = 2023
+DAY = 6
 
 puzzle = Puzzle(year=YEAR, day=DAY)
 
 
 # Part a
 def a(times, distances):
-    ttt = []
-    for t, d in zip(times, distances):
-        tt = []
-        for i in range(1, t + 1):
-            tt.append((t - i) * i)
-        tt = np.array(tt)
-        #breakpoint()
-        ttt.append(len(tt[tt > d])) #>=?
-    return np.prod(ttt)
-    breakpoint()
+    number_of_ways = []
+    for time_record, distance_record in zip(times, distances):
+        distance_traveled = []
+        for time_holding in range(1, time_record):
+            distance_traveled.append((time_record - time_holding) * time_holding)
+        distance_traveled = np.array(distance_traveled)
+        number_of_ways.append(len(distance_traveled[distance_traveled > distance_record]))
+    return np.prod(number_of_ways)
 
 
 example_answer = a(times=[7, 15, 30], distances=[9, 40, 200])
-#print(f"Example answer: {example_answer} (expecting: {example.answer_a})")
-#assert str(example_answer) == example.answer_a
-answer = a(times=[51  ,   69    , 98   ,  78], distances = [377  , 1171  , 1224 ,  1505])
+assert example_answer == 288
+answer = a(times=[51, 69, 98, 78], distances=[377, 1171, 1224, 1505])
 print("a:", answer)
-#puzzle.answer_a = answer
+assert answer == 131376
 
 
 # Part b
-def b(times, distances):
-    ttt = []
-    for t, d in zip(times, distances):
-        tt = []
-        for i in range(1, t + 1):
-            tt.append((t - i) * i)
-        tt = np.array(tt)
-        ttt.append(len(tt[tt > d]))
-    return np.prod(ttt)
-    breakpoint()
-
-
-example_answer = b(times=[71530], distances=[940200])
-#print(f"Example answer: {example_answer} (expecting: {example.answer_a})")
+example_answer = a(times=[71530], distances=[940200])
 assert example_answer == 71503
-answer = b(times=[51699878], distances = [377117112241505])
+answer = a(times=[51699878], distances=[377117112241505])
 print("b:", answer)
-puzzle.answer_b = answer
+assert answer == 34123437
