6e16bb7baec233f0f7577b1c4c5000b89284f5fe
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-12-07 09:51:03 +0100

    2023/07: Add solution for part b

--- 2023/07.py
+++ 2023/07.py
@@ -69,15 +69,52 @@ assert answer == 246912307
 
 
 # Part b
-def b(data):
-    breakpoint()
+SCORES_CARDS["J"] = 10
+
+SCORES_HANDS = {
+    # Five of a kind
+    (5, 0, 0): 36,
+    (4, 0, 1): 36,
+    (3, 0, 2): 36,
+    (2, 0, 3): 36,
+    (1, 0, 4): 36,
+    (0, 0, 5): 36,
+    # Four of a kind
+    (4, 1, 0): 35,
+    (3, 1, 1): 35,
+    (2, 1, 2): 35,
+    (1, 1, 3): 35,
+    # Full house
+    (3, 2, 0): 34,
+    (2, 2, 1): 34,
+    # Three of a kind
+    (3, 1, 0): 33,
+    (2, 1, 1): 33,
+    (1, 1, 2): 33,
+    # Two pairs
+    (2, 2, 0): 32,
+    # One pair
+    (2, 1, 0): 31,
+    (1, 1, 1): 31,
+    # High card
+    (1, 1, 0): 30,
+}
+
+
+def evaluate(hand):
+    counts = [hand.count(c) for c in SCORES_CARDS]
+    nbr_jokers = counts.pop(3)
+    counts = sorted(counts, reverse=True)
+    main_score = SCORES_HANDS[(*counts[:2], nbr_jokers)]
+    card_score = (SCORES_CARDS[c] for c in hand)
+    return int("".join(str(s) for s in [main_score, *card_score]))
 
 
 for example in puzzle.examples:
     if example.answer_b:
-        example_answer = b(example.input_data)
+        example_answer = a(example.input_data)
         print(f"Example answer: {example_answer} (expecting: {example.answer_b})")
         assert str(example_answer) == example.answer_b
-answer = b(puzzle.input_data)
+answer = a(puzzle.input_data)
 print("b:", answer)
 puzzle.answer_b = answer
