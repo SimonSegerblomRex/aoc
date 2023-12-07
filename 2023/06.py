7ef9e2c07b526c20b0c401e57d25fe2e8400c95e
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-12-07 19:54:29 +0100

    2023/06: Add sympy solution

--- 2023/06.py
+++ 2023/06.py
@@ -8,14 +8,52 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 
 
 # Part a
-def a(times, distances):
+def a_orig(times, distances):
     number_of_ways = []
     for time_record, distance_record in zip(times, distances):
         distance_traveled = []
         for time_holding in range(1, time_record):
             distance_traveled.append((time_record - time_holding) * time_holding)
         distance_traveled = np.array(distance_traveled)
-        number_of_ways.append(len(distance_traveled[distance_traveled > distance_record]))
+        number_of_ways.append(
+            len(distance_traveled[distance_traveled > distance_record])
+        )
+    return np.prod(number_of_ways)
+
+
+from sympy import Symbol, reduce_inequalities
+from sympy.core.relational import Relational
+from sympy.parsing.sympy_parser import parse_expr
+from sympy.solvers import solve
+
+
+def a(times, distances):
+    number_of_ways = []
+    # (t_r - t) * t >= d, t > 0
+    t = Symbol("t")
+    for t_r, d_r in zip(times, distances):
+        expr = parse_expr(f"({t_r} - t) * t > {d_r}, t > 0")
+        eq = reduce_inequalities(expr, t)
+        relations = [
+            (i.lhs, i.rel_op, i.rhs)
+            for i in [i.canonical for i in eq.atoms(Relational)]
+        ]
+        relations = sorted(relations, key=lambda x: float(x[2]))
+        for relation in relations:
+            if relation[1] == ">":
+                left_limit = int(relation[2] + 1)
+            elif relation[1] == ">=":
+                left_limit = int(relation[2] + 0.5)
+            elif relation[1] == "<":
+                if relation[2].is_Integer:
+                    right_limit = relation[2] - 1
+                else:
+                    right_limit = int(relation[2])
+            elif relation[1] == "<=":
+                right_limit = int(relation[2])
+            else:
+                raise ValueError("...")
+        number_of_ways.append(right_limit - left_limit + 1)
     return np.prod(number_of_ways)
 
 
