f759df98f1dcab8bb300ddaa0eee01ca3b1d29ae
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-12-11 06:50:33 +0100

    2023/11: Add solution for part b

--- 2023/11.py
+++ 2023/11.py
@@ -11,13 +11,12 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 
 
 # Part a
-def a(data):
+def a(data, extra):
     grid = np.vstack(
         [np.frombuffer(row.encode(), dtype=np.uint8) for row in data.splitlines()]
     )
     grid[grid == ord(".")] = 0
     grid[grid == ord("#")] = 1
-    #grid = np.pad(grid, 1, constant_values=ord("."))
     zero_rows = []
     for i, row in enumerate(range(grid.shape[0])):
         row = grid[i, :]
@@ -30,14 +29,26 @@ def a(data):
         if col.any():
             continue
         zero_cols.append(j)
-    grid = np.insert(grid, zero_rows, 0, axis=0)
-    grid = np.insert(grid, zero_cols, 0, axis=1)
+    #grid = np.insert(grid, zero_rows, 0, axis=0)
+    #grid = np.insert(grid, zero_cols, 0, axis=1)
     galaxies = np.nonzero(grid)
     from collections import defaultdict
     distances = defaultdict(lambda:np.inf)
     for i, galaxy_from in enumerate(zip(*galaxies)):
         for j, galaxy_to in enumerate(zip(*galaxies)):
-            d = np.abs(galaxy_from[0] - galaxy_to[0]) + np.abs(galaxy_from[1] - galaxy_to[1])
+            i_min = min(galaxy_from[0], galaxy_to[0])
+            i_max = max(galaxy_from[0], galaxy_to[0])
+            extra_i = 0
+            for r in zero_rows:
+                if i_min < r < i_max:
+                    extra_i += extra
+            j_min = min(galaxy_from[1], galaxy_to[1])
+            j_max = max(galaxy_from[1], galaxy_to[1])
+            extra_j = 0
+            for c in zero_cols:
+                if j_min < c < j_max:
+                    extra_j += extra
+            d = i_max + extra_i - i_min + j_max - j_min + extra_j
             if d:
                 idx = tuple(sorted((i, j)))
                 distances[idx] = min(distances[idx], d)
@@ -47,24 +58,21 @@ def a(data):
 
 for example in puzzle.examples:
     if example.answer_a:
-        example_answer = a(example.input_data)
+        example_answer = a(example.input_data, 1)
         print(f"Example answer: {example_answer} (expecting: {example.answer_a})")
         assert str(example_answer) == example.answer_a
-answer = a(puzzle.input_data)
+answer = a(puzzle.input_data, 1)
 print("a:", answer)
-puzzle.answer_a = answer
-
+assert answer == 9681886
 
 # Part b
-def b(data):
-    breakpoint()
+example_answer = a(example.input_data, 10 -1)
+assert example_answer == 1030
 
+example_answer = a(example.input_data, 100 - 1)
+assert example_answer == 8410
 
-for example in puzzle.examples:
-    if example.answer_b:
-        example_answer = b(example.input_data)
-        print(f"Example answer: {example_answer} (expecting: {example.answer_b})")
-        assert str(example_answer) == example.answer_b
-answer = b(puzzle.input_data)
+answer = a(puzzle.input_data, 1000000 - 1)
 print("b:", answer)
+assert answer != 791134890760
 puzzle.answer_b = answer
