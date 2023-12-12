e3393965c08c212c741d2bb34cf900bd8683b713
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-12-12 18:13:03 +0100

    2023/12: Not working b

--- 2023/12.py
+++ 2023/12.py
@@ -18,6 +18,8 @@ def a(data):
     for line in data.splitlines():
         c = 0
         springs, group_sizes = line.split(" ")
+        if not springs:
+            return 0
         group_sizes = np.fromstring(group_sizes, sep=",", dtype=int)
         nbr_damaged = sum(group_sizes)
         nbr_unknowns = springs.count("?")
@@ -34,30 +36,76 @@ def a(data):
                 c += 1
         total += c
     return total
-    breakpoint()
 
 
-example = """???.### 1,1,3
-.??..??...?##. 1,1,3
-?###???????? 3,2,1"""
-example_answer = a(example)
-
-
-answer = a(puzzle.input_data)
-print("a:", answer)
-puzzle.answer_a = answer
+examples = [
+    "???.### 1,1,3",
+    ".??..??...?##. 1,1,3",
+    "?#?#?#?#?#?#?#? 1,3,1,6",
+    "????.#...#... 4,1,1",
+    "????.######..#####. 1,6,5",
+    "?###???????? 3,2,1",
+]
+example_answers_a = [1, 4, 1, 1, 4, 10]
+for example, example_answer_a in zip(examples, example_answers_a):
+    example_answer = a(example)
+    print(f"Example answer: {example_answer} (expecting: {example_answer_a})")
+    assert example_answer == example_answer_a
+if 0:
+    answer = a(puzzle.input_data)
+    print("a:", answer)
+    assert answer == 7653
 
 
 # Part b
 def b(data):
-    breakpoint()
+    total = 0
+    for i, line in enumerate(data.splitlines()):
+        print(i)
+        c = 1
+        springs, group_sizes = line.split(" ")
+        springs = "?".join([springs]*5)
+        group_sizes = ",".join([group_sizes]*5)
+        group_sizes = np.fromstring(group_sizes, sep=",", dtype=int).tolist()
+        for group in springs.split("."):
+            if not group:
+                continue
+            if len(group) < group_sizes[0]:
+                continue
+            size = 0
+            subgroup_sizes = []
+            while True:
+                subgroup_size = group_sizes.pop(0)
+                subgroup_sizes.append(subgroup_size)
+                size += subgroup_size
+                if size + 1 >= len(group):
+                    break
+                if not group_sizes:
+                    break
+                size += 1
+            print(group, subgroup_sizes)
+            print("hmm:", f"{group} {','.join(str(i) for i in subgroup_sizes)}")
+            c *= a(f"{group} {','.join(str(i) for i in subgroup_sizes)}")
+            print(c)
+            if not group_sizes:
+                break
+        total += c
+    return total
 
 
-for example in puzzle.examples:
-    if example.answer_b:
-        example_answer = b(example.input_data)
-        print(f"Example answer: {example_answer} (expecting: {example.answer_b})")
-        assert str(example_answer) == example.answer_b
+examples = [
+    "???.### 1,1,3",
+    ".??..??...?##. 1,1,3",
+    "?#?#?#?#?#?#?#? 1,3,1,6",
+    "????.#...#... 4,1,1",
+    "????.######..#####. 1,6,5",
+    "?###???????? 3,2,1",
+]
+example_answers_b = [1, 16384, 1, 16, 2500, 506250]
+for example, example_answer_b in zip(examples, example_answers_b):
+    example_answer = b(example)
+    print(f"Example answer: {example_answer} (expecting: {example_answer_b})")
+    #assert example_answer == example_answer_b
 answer = b(puzzle.input_data)
 print("b:", answer)
-puzzle.answer_b = answer
+#puzzle.answer_b = answer
