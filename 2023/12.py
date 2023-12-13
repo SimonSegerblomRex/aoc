f8e290480ad912f4ab299986a3dfc53c882ccadb
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-12-13 20:25:28 +0100

    2023/12: Replace slow solution for a with the one from b

--- 2023/12.py
+++ 2023/12.py
@@ -1,8 +1,6 @@
 from functools import cache
 
-import numpy as np
 from aocd.models import Puzzle
-from more_itertools import distinct_permutations
 
 YEAR = 2023
 DAY = 12
@@ -11,53 +9,6 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 
 
 # Part a
-def a(data):
-    total = 0
-    for line in data.splitlines():
-        c = 0
-        springs, group_sizes = line.split(" ")
-        if not springs:
-            return 1
-        group_sizes = np.fromstring(group_sizes, sep=",", dtype=int)
-        nbr_damaged = sum(group_sizes)
-        nbr_unknowns = springs.count("?")
-        nbr_damaged_in_unknowns = nbr_damaged - springs.count("#")
-        nbr_operational_in_unknowns = nbr_unknowns - nbr_damaged_in_unknowns
-        for combo in distinct_permutations(
-            "#" * nbr_damaged_in_unknowns + "." * nbr_operational_in_unknowns
-        ):
-            tmp = np.array(list(springs))
-            tmp[tmp == "?"] = combo
-            tmp = "".join(tmp)
-            groups = [g for g in tmp.split(".") if g]
-            sizes = [len(g) for g in groups]
-            if (len(group_sizes) == len(sizes)) and (group_sizes == sizes).all():
-                # print(combo, tmp)
-                c += 1
-        total += c
-    return total
-
-
-examples = [
-    "???.### 1,1,3",
-    ".??..??...?##. 1,1,3",
-    "?#?#?#?#?#?#?#? 1,3,1,6",
-    "????.#...#... 4,1,1",
-    "????.######..#####. 1,6,5",
-    "?###???????? 3,2,1",
-]
-example_answers_a = [1, 4, 1, 1, 4, 10]
-for example, example_answer_a in zip(examples, example_answers_a):
-    example_answer = a(example)
-    print(f"Example answer: {example_answer} (expecting: {example_answer_a})")
-    assert example_answer == example_answer_a
-if 0:
-    answer = a(puzzle.input_data)
-    print("a:", answer)
-    assert answer == 7653
-
-
-# Part b
 @cache
 def combos(springs, sizes, curr_size, curr_target):
     springs = list(springs)
@@ -95,6 +46,35 @@ def combos(springs, sizes, curr_size, curr_target):
     return 1
 
 
+def a(data):
+    total = 0
+    for line in data.splitlines():
+        springs, group_sizes = line.split(" ")
+        group_sizes = list(map(int, group_sizes.split(",")))
+        target = group_sizes.pop(0)
+        total += combos(tuple(springs), tuple(group_sizes), 0, target)
+    return total
+
+
+examples = [
+    "???.### 1,1,3",
+    ".??..??...?##. 1,1,3",
+    "?#?#?#?#?#?#?#? 1,3,1,6",
+    "????.#...#... 4,1,1",
+    "????.######..#####. 1,6,5",
+    "?###???????? 3,2,1",
+]
+example_answers_a = [1, 4, 1, 1, 4, 10]
+for example, example_answer_a in zip(examples, example_answers_a):
+    example_answer = a(example)
+    print(f"Example answer: {example_answer} (expecting: {example_answer_a})")
+    assert example_answer == example_answer_a
+answer = a(puzzle.input_data)
+print("a:", answer)
+assert answer == 7653
+
+
+# Part b
 def b(data):
     total = 0
     for line in data.splitlines():
@@ -108,11 +88,11 @@ def b(data):
 
 examples = [
     "???.### 1,1,3",
-    ".??..??...?##. 1,1,3",  # 4*8**4
+    ".??..??...?##. 1,1,3",
     "?#?#?#?#?#?#?#? 1,3,1,6",
     "????.#...#... 4,1,1",
-    "????.######..#####. 1,6,5",  # 4*5**4
-    "?###???????? 3,2,1",  # Note: 10*15**4
+    "????.######..#####. 1,6,5",
+    "?###???????? 3,2,1",
 ]
 example_answers_b = [1, 16384, 1, 16, 2500, 506250]
 for example, example_answer_b in zip(examples, example_answers_b):
