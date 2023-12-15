704904c0c59f8188de89f01edc102048772a19e8
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-12-15 06:07:02 +0100

    2023/15: Add solution for part a

---
+++ 2023/15.py
@@ -0,0 +1,49 @@
+import datetime
+import re
+
+import numpy as np
+from aocd.models import Puzzle
+
+YEAR = datetime.datetime.today().year
+DAY = datetime.datetime.today().day
+
+puzzle = Puzzle(year=YEAR, day=DAY)
+
+
+# Part a
+def a(data):
+    steps = data.split(",")
+    t = 0
+    for s in steps:
+        v = 0
+        for c in s:
+            v += ord(c)
+            v *= 17
+            v %= 256
+        t += v
+    return t
+
+
+for example in puzzle.examples:
+    if example.answer_a:
+        example_answer = a(example.input_data)
+        print(f"Example answer: {example_answer} (expecting: {example.answer_a})")
+        assert str(example_answer) == example.answer_a
+answer = a(puzzle.input_data)
+print("a:", answer)
+puzzle.answer_a = answer
+
+
+# Part b
+def b(data):
+    breakpoint()
+
+
+for example in puzzle.examples:
+    if example.answer_b:
+        example_answer = b(example.input_data)
+        print(f"Example answer: {example_answer} (expecting: {example.answer_b})")
+        assert str(example_answer) == example.answer_b
+answer = b(puzzle.input_data)
+print("b:", answer)
+puzzle.answer_b = answer
