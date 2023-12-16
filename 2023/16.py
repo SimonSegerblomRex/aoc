358c0bbeb8279152ec886eedb763df38ffb60b6d
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-12-16 10:50:40 +0100

    2023/16: Minor cleanup

--- 2023/16.py
+++ 2023/16.py
@@ -8,23 +8,15 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 
 # Part a
 def get_new_beams(pos, dir, kind):
-    if kind == ".":
-        return [(pos + dir, dir)]
-    if kind == "|":
-        if dir in (0 + 1j, 0 - 1j):
-            return [(pos + 1, 1 + 0j), (pos - 1, -1 + 0j)]
-        return [(pos + dir, dir)]
-    if kind == "-":
-        if dir in (1 + 0j, -1 + 0j):
-            return [(pos - 1j, 0 - 1j), (pos + 1j, 0 + 1j)]
-        return [(pos + dir, dir)]
+    if kind == "|" and dir in (0 + 1j, 0 - 1j):
+        return [(pos + 1, 1 + 0j), (pos - 1, -1 + 0j)]
+    if kind == "-" and dir in (1 + 0j, -1 + 0j):
+        return [(pos - 1j, 0 - 1j), (pos + 1j, 0 + 1j)]
     if kind == "/":
-        new_dir = complex(-dir.imag, -dir.real)
-        return [(pos + new_dir, new_dir)]
-    if kind == "\\":
-        new_dir = complex(dir.imag, dir.real)
-        return [(pos + new_dir, new_dir)]
-    raise ValueError
+        dir = complex(-dir.imag, -dir.real)
+    elif kind == "\\":
+        dir = complex(dir.imag, dir.real)
+    return [(pos + dir, dir)]
 
 
 def create_grid(rows):
@@ -43,12 +35,12 @@ def count_visited(grid, height, width, start):
     visited = set()
     while beams:
         pos, dir = beams.pop()
-        if pos.real < 0 or pos.real > height - 1 or pos.imag < 0 or pos.imag > width - 1:
+        if not ((0 <= pos.real < height) and (0 <= pos.imag < width)):
             continue
         visited.add((pos, dir))
         new_beams = grid[(pos, dir)]
         beams.update(new_beams)
-        beams.difference_update(visited)
+        beams -= visited
     return len(set(p for p, _ in visited))
 
 
@@ -78,18 +70,17 @@ def b(data):
     counts = []
     for i in range(0, height):
         counts.append(count_visited(grid, height, width, (complex(i, 0), 0 + 1j)))
-        counts.append(count_visited(grid, height, width, (complex(i, width - 1), 0 - 1j)))
+        counts.append(
+            count_visited(grid, height, width, (complex(i, width - 1), 0 - 1j))
+        )
     for j in range(0, width):
         counts.append(count_visited(grid, height, width, (complex(0, j), 1 + 0j)))
-        counts.append(count_visited(grid, height, width, (complex(height - 1, j), -1 + 0j)))
+        counts.append(
+            count_visited(grid, height, width, (complex(height - 1, j), -1 + 0j))
+        )
     return max(counts)
 
 
-for example in puzzle.examples:
-    if example.answer_b:
-        example_answer = b(example.input_data)
-        print(f"Example answer: {example_answer} (expecting: {example.answer_b})")
-        assert str(example_answer) == example.answer_b
 answer = b(puzzle.input_data)
 print("b:", answer)
-puzzle.answer_b = answer
+assert answer == 8383
