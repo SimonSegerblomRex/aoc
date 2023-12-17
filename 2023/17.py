78e8ed00e3909087e985145d7e35140cd653c516
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-12-17 12:55:52 +0100

    2023/17: Add solution for b

--- 2023/17.py
+++ 2023/17.py
@@ -21,7 +21,7 @@ def create_grid(rows):
     return grid
 
 
-def a_star(start, end, grid, hist=3):
+def a_star(start, end, grid):
     def h(node):
         return int(abs(end.real - node.real) + abs(end.imag - node.imag))
 
@@ -76,22 +76,70 @@ for example in puzzle.examples:
         example_answer = a(example.input_data)
         print(f"Example answer: {example_answer} (expecting: {example.answer_a})")
         assert str(example_answer) == example.answer_a
-answer = a(puzzle.input_data)
-assert answer > 956
-print("a:", answer)
-puzzle.answer_a = answer
-
+if 0:
+    answer = a(puzzle.input_data)
+    print("a:", answer)
+    assert answer == 959
 
 # Part b
+def a_star_b(start, end, grid):
+    def h(node):
+        return int(abs(end.real - node.real) + abs(end.imag - node.imag))
+
+    open_set = queue.PriorityQueue()
+    open_set.put((0, 0, start, 0 + 1j, 1))
+    open_set.put((0, 1, start, 1 + 0j, 1))
+
+    g_score = defaultdict(lambda: 1 << 30)
+    g_score[(start, 0 + 1j, 1)] = 0
+    g_score[(start, 1 + 0j, 1)] = 0
+
+    f_score = defaultdict(lambda: 1 << 30)
+    f_score[(start, 0 + 1j, 1)] = h(start)
+    f_score[(start, 1 + 0j, 1)] = h(start)
+
+    i = 2
+    while not open_set.empty():
+        _, _, current, prev_dir, count = open_set.get()
+        if current == end:
+            return int(g_score[(current, prev_dir, count)])
+
+        dirs = [0 + 1j, -1 + 0j, 0 - 1j, 1 + 0j]
+        dirs.remove(-prev_dir)
+        for dir in dirs:
+            next_node = current + dir
+            if next_node not in grid:
+                continue
+            if prev_dir == dir:
+                next_count = count + 1
+            else:
+                if count >= 4:
+                    next_count = 1
+                else:
+                    continue
+            if next_count > 10:
+                continue
+            tentative_g_score = g_score[(current, prev_dir, count)] + grid[next_node]
+            if tentative_g_score < g_score[(next_node, dir, next_count)]:
+                g_score[(next_node, dir, next_count)] = tentative_g_score
+                f_score[(next_node, dir, next_count)] = tentative_g_score + h(next_node)
+                open_set.put((f_score[(next_node, dir, next_count)], i, next_node, dir, next_count))
+                i += 1
+
+
 def b(data):
-    breakpoint()
+    rows = data.splitlines()
+    grid = create_grid(rows)
+    start = list(grid)[0]
+    end = list(grid)[-1]
+    return a_star_b(start, end, grid)
 
 
 for example in puzzle.examples:
     if example.answer_b:
         example_answer = b(example.input_data)
-        print(f"Example answer: {example_answer} (expecting: {example.answer_b})")
-        assert str(example_answer) == example.answer_b
+        print(f"Example answer: {example_answer} (expecting: {94})")
+        assert example_answer == 94
 answer = b(puzzle.input_data)
 print("b:", answer)
 puzzle.answer_b = answer
