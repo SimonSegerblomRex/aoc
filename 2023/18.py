85350c7661bdf5d1958753933b56a005247d5cff
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-12-18 20:03:58 +0100

    2023/18: Add solution for part b

--- 2023/18.py
+++ 2023/18.py
@@ -14,6 +14,19 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 
 
 # Part a
+def calculate_area(corners):
+    edge = 0
+    for i in range(1, len(corners)):
+        edge +=abs(corners[i - 1] - corners[i])
+
+    area = 0
+    for i in range(1, len(corners)):
+        area += int(corners[i].real) * (int(corners[i].imag) - int(corners[i-1].imag))
+    area = abs(area)
+
+    return area + int(edge) // 2 + 1
+
+
 def a(data):
     corners = [0 + 0]
     dirs = {
@@ -28,17 +41,7 @@ def a(data):
         curr += dirs[dir] * int(steps)
         corners.append(curr)
     corners.append(corners[0])
-
-    edge = 0
-    for i in range(1, len(corners)):
-        edge +=abs(corners[i - 1] - corners[i])
-
-    area = 0
-    for i in range(1, len(corners)):
-        area += int(corners[i].real) * (int(corners[i].imag) - int(corners[i-1].imag))
-    area = abs(area)
-
-    return area + int(edge) // 2 + 1
+    return calculate_area(corners)
 
 
 for example in puzzle.examples:
@@ -53,7 +56,22 @@ assert answer == 48400
 
 # Part b
 def b(data):
-    breakpoint()
+    corners = [0 + 0]
+    dirs = {
+        "0": 0 + 1j,
+        "3": -1 + 0j,
+        "2": 0 - 1j,
+        "1": 1 + 0j,
+    }
+    curr = corners[0]
+    for line in data.splitlines():
+        _, _, hex_code = line.split(" ")
+        steps = int(hex_code[2:7], 16)
+        dir = hex_code[7]
+        curr += dirs[dir] * int(steps)
+        corners.append(curr)
+    corners.append(corners[0])
+    return calculate_area(corners)
 
 
 for example in puzzle.examples:
