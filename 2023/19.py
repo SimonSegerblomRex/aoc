9832f4423e9a824e23d9a54a3fa70b549e20ffaf
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-12-19 07:34:46 +0100

    2023/19: Kladd b

--- 2023/19.py
+++ 2023/19.py
@@ -1,5 +1,6 @@
 import datetime
 import re
+from collections import defaultdict
 
 import numpy as np
 from aocd.models import Puzzle
@@ -57,19 +58,86 @@ for example in puzzle.examples:
         assert str(example_answer) == example.answer_a
 answer = a(puzzle.input_data)
 print("a:", answer)
-puzzle.answer_a = answer
+assert answer == 350678
 
 
 # Part b
 def b(data):
-    breakpoint()
+    workflows_input, ratings_input = data.split("\n\n")
+    workflows = {}
+    for line in workflows_input.splitlines():
+        name, rules = line.split("{")
+        rules = rules.strip("}").split(",")
+        workflows[name] = rules
+    s = 0
+    for line in ratings_input.splitlines():
+        curr_workflow = "in"
+        hard_min_limits = {"x": 1, "m": 1, "a": 1, "s": 1}
+        hard_max_limits = {"x": 4000, "m": 4000, "a": 4000, "s": 4000}
+        soft_min_limits = defaultdict(lambda: 0)
+        soft_max_limits = defaultdict(lambda: 0)
+        while True:
+            rules = workflows[curr_workflow]
+            for rule in rules:
+                if "<" in rule:
+                    c, v = rule.split("<")
+                    v, dest = v.split(":")
+                    print(dest)
+                    if dest == "R":
+                        hard_max_limits[c] = min(hard_max_limits[c], int(v) - 1)
+                        continue
+                    elif dest == "A":
+                        soft_max_limits[c] = max(soft_max_limits[c] or int(v) - 1, int(v) - 1)
+                        continue
+                    curr_workflow = dest
+                    break
+                elif ">" in rule:
+                    c, v = rule.split(">")
+                    v, dest = v.split(":")
+                    if dest == "R":
+                        hard_min_limits[c] = max(hard_min_limits[c], int(v) + 1)
+                        continue
+                    elif dest == "A":
+                        soft_min_limits[c] = min(soft_min_limits[c] or int(v) + 1, int(v) + 1)
+                        continue
+                    curr_workflow = dest
+                    break
+                else:
+                    curr_workflow = rule
+            if curr_workflow == "A":
+                print("!!!!!!!!!!!!!!!!!!!!!")
+                #breakpoint()
+                break
+            elif curr_workflow == "R":
+                tmp = []
+                print(list(reversed(soft_min_limits.items())))
+                print(list(reversed(soft_max_limits.items())))
+                #breakpoint()
+                for l, u in zip(soft_min_limits.values(), soft_max_limits.values()):
+                    if not l and not u:
+                        continue
+                    print(soft_min_limits, soft_min_limits)
+                    l = l or 1
+                    u = u or 4000
+                    if u >= l:
+                        tmp.append((u - l + 1))
+                s += np.array(tmp).prod()
+                break
+            """
+                tmp = []
+                for l, u in zip(min_limits.values(), max_limits.values()):
+                    if u >= l:
+                        tmp.append((u - l + 1))
+                print(tmp)
+                s += np.array(tmp).prod()
+            """
+    return s
 
 
 for example in puzzle.examples:
-    if example.answer_b:
-        example_answer = b(example.input_data)
-        print(f"Example answer: {example_answer} (expecting: {example.answer_b})")
-        assert str(example_answer) == example.answer_b
+    example_answer = b(example.input_data)
+    print(f"Example answer: {example_answer} (expecting: {example.answer_b})")
+    assert str(example_answer) == example.answer_b
 answer = b(puzzle.input_data)
 print("b:", answer)
 puzzle.answer_b = answer
