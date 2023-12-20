77197a235798f48a4af7a2c4a1968433423ac91e
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-12-20 08:11:35 +0100

    2023/20: Kladd for b

--- 2023/20.py
+++ 2023/20.py
@@ -40,6 +40,8 @@ def send_pulse_old(modules, sender, receiver, pulse):
 
 def send_pulse(modules, sender, receiver, pulse):
     #print(sender, pulse, receiver)
+    if receiver == "rx" and pulse == 0:
+        return [8]
     if receiver not in modules:
         return []
     m = modules[receiver]
@@ -121,19 +123,61 @@ assert example_answer == 11687500
 answer = a(puzzle.input_data)
 print("a:", answer)
 assert answer < 850401079
-puzzle.answer_a = answer
+#puzzle.answer_a = answer
 
 
 # Part b
 def b(data):
-    breakpoint()
+    modules = {}
+    for line in data.splitlines():
+        items = line.split(" -> ")
+        module = items[0]
+        items = items[1].split(",")
+        items = [item.strip() for item in items]
+        if module in ("broadcaster"):
+            modules[module] = {
+                "type": module,
+                "dest": items,
+            }
+        else:
+            typ = module[0]
+            modules[module[1:]] = {
+                "type": typ,
+                "dest": items,
+            }
+            if typ == "%":
+                modules[module[1:]]["state"] = 0
+    for module in modules:
+        if modules[module]["type"] == "&":
+            modules[module]["hist"] = {}
+            for m in modules:
+                if module in modules[m]["dest"]:
+                    modules[module]["hist"][m] = 0
+    """
+    pulses = [0, 0]
+    for _ in range(1000):
+        low, high = send_pulse(modules, "button", "broadcaster", 0)
+        pulses[0] += 1
+        pulses[0] += low
+        pulses[1] += high
+        breakpoint()
+    """
+    pulses = [0, 0]
+    curr_pulse = 0
+    modules_to_process = [("button", "broadcaster", 0)]
+    i = 1
+    while True:
+        modules_to_process = [("button", "broadcaster", 0)]
+        while modules_to_process:
+            sender, receiver, pulse = modules_to_process.pop(0)
+            pulses[pulse] += 1
+            tmp = send_pulse(modules, sender, receiver, pulse)
+            if 8 in tmp:
+                return i
+            i += 1
+            modules_to_process.extend(tmp)
 
 
-for example in puzzle.examples:
-    if example.answer_b:
-        example_answer = b(example.input_data)
-        print(f"Example answer: {example_answer} (expecting: {example.answer_b})")
-        assert str(example_answer) == example.answer_b
 answer = b(puzzle.input_data)
 print("b:", answer)
 puzzle.answer_b = answer
