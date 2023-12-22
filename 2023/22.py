2efa44c10743434ecd558895701a30fbaa1bc710
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-12-22 21:42:28 +0100

    2023/22: Cleanup

--- 2023/22.py
+++ 2023/22.py
@@ -10,7 +10,7 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 
 
 # Part a
-def a(data):
+def get_neighbours(data):
     bricks = []
     max_z = 0
     for line in data.splitlines():
@@ -18,35 +18,49 @@ def a(data):
         c0 = list(ast.literal_eval(c0))
         c1 = list(ast.literal_eval(c1))
         bricks.append([c0, c1])
-        max_z = max(max_z, c0[2])
-        max_z = max(max_z, c1[2])
+        max_z = max(max_z, c0[2], c1[2])
 
-    z = np.zeros((12+10,12+10,max_z+10), dtype=int)
-    z[:,:,0] = 1 #ground
+    z = np.zeros((10, 10, max_z), dtype=int)
+    z[:, :, 0] = 1  # ground
     bricks = sorted(bricks, key=lambda b: min(b[0][2], b[1][2]))
 
     n = 1
     for c0, c1 in bricks:
         n += 1
-        lowest_z = z.argmax(axis=2)[c0[0]:c1[0]+1,c0[1]:c1[1]+1].max() + 1
+        lowest_z = z.argmax(axis=2)[c0[0] : c1[0] + 1, c0[1] : c1[1] + 1].max() + 1
         while True:
-            if z[c0[0]:c1[0]+1,c0[1]:c1[1]+1,lowest_z:lowest_z+c1[2]-c0[2]+1].sum() > 0:
+            if (
+                z[
+                    c0[0] : c1[0] + 1,
+                    c0[1] : c1[1] + 1,
+                    lowest_z : lowest_z + c1[2] - c0[2] + 1,
+                ].sum()
+                > 0
+            ):
                 lowest_z += 1
             else:
                 break
-        z[c0[0]:c1[0]+1,c0[1]:c1[1]+1,lowest_z:lowest_z+c1[2]-c0[2]+1] = n
+        z[
+            c0[0] : c1[0] + 1,
+            c0[1] : c1[1] + 1,
+            lowest_z : lowest_z + c1[2] - c0[2] + 1,
+        ] = n
         c0[2] = lowest_z
         c1[2] = lowest_z + c1[2] - c0[2]
 
     neighbours = {}
     for n in range(2, n + 1):
         neigh = set()
-        for c in zip(*np.nonzero(z==n)):
+        for c in zip(*np.nonzero(z == n)):
             c = (c[0], c[1], c[2] + 1)
             if z[c] > n:
                 neigh.add(z[c])
         neighbours[n] = neigh
+    return neighbours
 
+
+def a(data):
+    neighbours = get_neighbours(data)
     s = 0
     for n, neigh in neighbours.items():
         others = neighbours.copy()
@@ -54,7 +68,6 @@ def a(data):
         tmp = neigh.difference(*(list(s) for s in others.values()))
         if not tmp:
             s += 1
-
     return s
 
 
@@ -69,63 +82,25 @@ assert answer == 482
 
 
 # Part b
-def b(data):
-    bricks = []
-    max_z = 0
-    for line in data.splitlines():
-        c0, c1 = line.split("~")
-        c0 = list(ast.literal_eval(c0))
-        c1 = list(ast.literal_eval(c1))
-        bricks.append([c0, c1])
-        max_z = max(max_z, c0[2])
-        max_z = max(max_z, c1[2])
-
-    z = np.zeros((12+10,12+10,max_z+10), dtype=int)
-    z[:,:,0] = 1 #ground
-    bricks = sorted(bricks, key=lambda b: min(b[0][2], b[1][2]))
-
-    n = 1
-    for c0, c1 in bricks:
-        n += 1
-        lowest_z = z.argmax(axis=2)[c0[0]:c1[0]+1,c0[1]:c1[1]+1].max() + 1
-        if 1:
-            while True:
-                if z[c0[0]:c1[0]+1,c0[1]:c1[1]+1,lowest_z:lowest_z+c1[2]-c0[2]+1].sum() > 0:
-                    lowest_z += 1
-                else:
-                    break
-        z[c0[0]:c1[0]+1,c0[1]:c1[1]+1,lowest_z:lowest_z+c1[2]-c0[2]+1] = n
-        c0[2] = lowest_z
-        c1[2] = lowest_z + c1[2] - c0[2]
-
-    neighbours = {}
-    nn = n
-    for n in range(2, n + 1):
-        neigh = set()
-        for c in zip(*np.nonzero(z==n)):
-            c = (c[0], c[1], c[2] + 1)
-            if z[c] > n:
-                neigh.add(z[c])
-        neighbours[n] = neigh
-
-    def disintegrate(blocks, neighbours):
-        s = set()
-        neigh = set()
-        for n in blocks:
-            neigh |= neighbours.pop(n)
-        if not neigh:
-            return s
-        neigh = neigh.copy()
-        neigh ^= neigh.intersection(set.union(*(s for s in neighbours.values())))
-        if neigh:
-            s |= neigh
-            s |= disintegrate(neigh, neighbours.copy())
+def disintegrate(blocks, neighbours):
+    s = set()
+    neigh = set()
+    for n in blocks:
+        neigh |= neighbours.pop(n)
+    if not neigh:
         return s
+    neigh ^= neigh.intersection(set.union(*(s for s in neighbours.values())))
+    if neigh:
+        s |= neigh
+        s |= disintegrate(neigh, neighbours.copy())
+    return s
 
+
+def b(data):
+    neighbours = get_neighbours(data)
     s = 0
-    for n, neigh in neighbours.items():
-        tmp = disintegrate(set([n]), neighbours.copy())
-        s += (np.array(list(tmp)) > n).sum()
+    for n in neighbours:
+        s += len(disintegrate(set([n]), neighbours.copy()))
     return s
 
 
