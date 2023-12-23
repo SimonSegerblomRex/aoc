2c05ce57e50d6890bed4d0fdb45f6e81f255b95c
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2023-12-23 21:05:31 +0100

    2023/23: Ugly brute-forece mess for part p

--- 2023/23.py
+++ 2023/23.py
@@ -126,8 +126,6 @@ def c_star(path, end, forest, slopes):
     return best_path
 
 
-forest = []
-
 @cache
 def d_star(path, end):
     if path[-1] == end:
@@ -151,18 +149,174 @@ def d_star(path, end):
                 best_path = p
     return best_path
 
-def b(data):
-    grid = np.vstack(
-        [np.frombuffer(row.encode(), dtype=np.uint8) for row in data.splitlines()]
-    )
-    height, width = grid.shape
-    grid[grid != ord("#")] = 0
-    grid[grid == ord("#")] = 3
-    grid[0, 1] = 3
-    grid[0, 1] = 3
-    grid[height-1, width-2] = 3
 
-    if 0:
+def find_connections(node, forest):
+    dirs = [0 + 1j, -1 + 0j, 0 - 1j, 1 + 0j]
+    connections = set()
+    search_dirs = [dir for dir in dirs if node + dir not in forest]
+    for search_dir in search_dirs:
+        current = node
+        next_dir = search_dir
+        s = 0
+        while True:
+            next_node = current + next_dir
+            s += 1
+            next_dirs = [dir for dir in dirs if next_node + dir not in forest and dir != -next_dir]
+            if len(next_dirs) != 1:
+                break
+            next_dir = next_dirs[0]
+            current = next_node
+        if 1:#len(next_dirs) > 1:
+            connections.add((next_node, s))
+    return connections
+
+
+def create_graph(start, goal, forest):
+    graph = {}
+    nodes = set([start])
+    while True:
+        new_nodes = set()
+        for node in nodes:
+            if node not in graph:
+                connections = find_connections(node, forest)
+                connections = connections#{c for c in connections if c[0] not in graph}
+                if connections:
+                    graph[node] = connections
+                    new_nodes |= set([n for n, _ in connections])
+        if not new_nodes:
+            break
+        nodes = new_nodes
+    return graph
+
+
+def e_star(paths, goal, graph):
+    if sum(p[-1] == goal for (p, _) in paths) == len(paths):
+        return paths
+    new_paths = []
+    for (path, steps) in paths:
+        current = path[-1]
+        if current == goal:
+            new_paths.append((path, steps))
+            continue
+        for (next_node, s) in graph[current]:
+            if next_node in path:
+                continue
+            new_paths.append(([*path, next_node], steps+s))
+    return e_star(new_paths, goal, graph)
+
+
+global graph
+@cache
+def f_star(paths, goal):
+    if sum(p[-1] == goal for (p, _) in paths) == len(paths):
+        return paths
+    new_paths = []
+    for (path, steps) in paths:
+        current = path[-1]
+        if current == goal:
+            new_paths.append((path, steps))
+            continue
+        for (next_node, s) in graph[current]:
+            if next_node in path:
+                continue
+            new_paths.append(((*path, next_node), steps + s))
+    return f_star(tuple(new_paths), goal)
+
+
+
+def a_star(start, goal, graph):
+    def h(node):
+        return int(abs(goal.real - node.real) + abs(goal.imag - node.imag))
+
+    open_set = queue.PriorityQueue()
+    open_set.put((0, 0, start))
+
+    g_score = defaultdict(lambda: 0)
+    g_score[start] = 0
+    f_score = defaultdict(lambda: 0)
+    f_score[start] = 0
+
+    i = 1
+    while not open_set.empty():
+        _, _,current = open_set.get()
+        for node, s in graph[current]:
+            tentative_g_score = g_score[current] + s
+            if tentative_g_score >  g_score[node]:
+                g_score[node] = tentative_g_score
+                f_score[node] = tentative_g_score + 1#h(node)
+                open_set.put((-f_score[node],i,node))
+                i += 1
+    breakpoint()
+
+def b(data):
+    forest = []
+    for i, line in enumerate(data.splitlines()):
+        for m in re.finditer("#", line):
+            j = m.start()
+            forest.append(complex(i, j))
+    height, width = i + 1, j + 1
+
+    start = 0 + 1j
+
+    goal = complex(height - 1, width - 2)
+
+    forest.append(start - 1)
+    forest.append(goal + 1)
+
+    #graph = create_dag(start, goal, forest)
+    #tmp = a_star(start, goal, graph)
+    global graph
+    graph = create_graph(start, goal, forest)
+    import networkx as nx
+    G = nx.Graph()
+    for from_node, to_nodes in graph.items():
+        for to_node, step in to_nodes:
+            G.add_edge(from_node, to_node, weight=step)
+    t2 = nx.all_simple_paths(G, source=start, target=goal)
+    m = 0
+    for path in t2:
+        m = max(m, nx.path_weight(G, path, weight="weight"))
+    return m
+    #t3 = next(t2)
+
+    breakpoint()
+    #add_edge("A", "B", weight=4)
+
+    paths = f_star((((start,), 0),), goal)
+    breakpoint()
+    #paths = e_star([([start], 0)], goal, graph)
+    return max(s for _, s in paths)
+    #print(max(len(p) for p in paths) - 1)
+    breakpoint()
+    tmp = a_star(goal, start, graph)
+    breakpoint()
+
+    #path = d_star((start,), goal)
+    #return len(path) - 1
+
+
+
+example_answer = b(example.input_data)
+print(f"Example answer: {example_answer} (expecting: {154})")
+#assert example_answer == 154
+answer = b(puzzle.input_data)
+print("b:", answer)
+assert answer > 2000
+puzzle.answer_b = answer
+
+
+"""
+   if 0:
+        grid = np.vstack(
+            [np.frombuffer(row.encode(), dtype=np.uint8) for row in data.splitlines()]
+        )
+        height, width = grid.shape
+        grid[grid != ord("#")] = 0
+        grid[grid == ord("#")] = 3
+        grid[0, 1] = 3
+        grid[0, 1] = 3
+        grid[height-1, width-2] = 3
+
         if 0:
             from scipy.ndimage import watershed_ift
             markers = grid.copy().astype(int)
@@ -191,42 +345,4 @@ def b(data):
             print("")
 
         breakpoint()
-
-    global forest
-    forest = []
-    slopes = {}
-    dirs = {
-        "^": -1 + 0j,
-        ">": 0 + 1j,
-        "v": 1 + 0j,
-        "<": 0 - 1j,
-    }
-    for i, line in enumerate(data.splitlines()):
-        for m in re.finditer("#", line):
-            j = m.start()
-            forest.append(complex(i, j))
-        for m in re.finditer("\^|>|v|<", line):
-            j = m.start()
-            slopes[complex(i, j)] = dirs[m.group(0)]
-    height, width = i + 1, j + 1
-
-    start = 0 + 1j
-    dir = 1 + 0j
-
-    goal = complex(height - 1, width - 2)
-
-    forest.append(start - 1)
-    forest.append(goal + 1)
-
-    path = d_star((start,), goal)
-    return len(path) - 1
-
-
-
-example_answer = b(example.input_data)
-print(f"Example answer: {example_answer} (expecting: {154})")
-assert example_answer == 154
-answer = b(puzzle.input_data)
-print("b:", answer)
-assert answer > 2000
-puzzle.answer_b = answer
+"""
