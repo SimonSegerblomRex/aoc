9d1c1981df41263bb583bfc868228ea50b645875
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-11-03 08:20:48 +0100

    2019/05: Add solution for part b

--- 2019/05.py
+++ 2019/05.py
@@ -49,34 +49,72 @@ puzzle.answer_a = answer
 
 # Part b
 def b(data):
-    codes_orig = list(map(int, data.split(",")))
-    for noun in range(100):
-        for verb in range(100):
-            codes = codes_orig.copy()
-            codes[1] = noun
-            codes[2] = verb
-            pos = 0
-            if noun == 99:
-                print(noun, verb)
-            try:
-                while True:
-                    opcode = codes[pos]
-                    if opcode == 1:
-                        codes[codes[pos + 3]] = codes[codes[pos + 1]] + codes[codes[pos + 2]]
-                        pos += 4
-                    elif opcode == 2:
-                        codes[codes[pos + 3]] = codes[codes[pos + 1]] * codes[codes[pos + 2]]
-                        pos += 4
-                    elif opcode == 99:
-                        break
-                    else:
-                        break
-            except IndexError:
-                continue
-            if codes[0] == 19690720:
-                return 100 * noun + verb
+    codes = list(map(int, data.split(",")))
+    pos = 0
+    inp = 5
+    while True:
+        instruction = codes[pos]
+        opcode = instruction % 100
+        if opcode == 1:
+            idx0 = pos + 1 if (instruction // 100) % 2 else codes[pos + 1]
+            idx1 = pos + 2 if (instruction // 1000) % 2 else codes[pos + 2]
+            idx2 = pos + 3 if (instruction // 10000) % 2 else codes[pos + 3]
+            codes[idx2] = codes[idx0] + codes[idx1]
+            pos += 4
+        elif opcode == 2:
+            idx0 = pos + 1 if (instruction // 100) % 2 else codes[pos + 1]
+            idx1 = pos + 2 if (instruction // 1000) % 2 else codes[pos + 2]
+            idx2 = pos + 3 if (instruction // 10000) % 2 else codes[pos + 3]
+            codes[idx2] = codes[idx0] * codes[idx1]
+            pos += 4
+        elif opcode == 3:
+            idx0 =  pos + 1 if (instruction // 100) % 2 else codes[pos + 1]
+            codes[idx0] = inp
+            pos += 2
+        elif opcode == 4:
+            idx0 =  pos + 1 if (instruction // 100) % 2 else codes[pos + 1]
+            inp = codes[idx0]
+            pos += 2
+        elif opcode == 5:
+            idx0 = pos + 1 if (instruction // 100) % 2 else codes[pos + 1]
+            idx1 = pos + 2 if (instruction // 1000) % 2 else codes[pos + 2]
+            if codes[idx0] != 0:
+                pos = codes[idx1]
+            else:
+                pos += 3
+        elif opcode == 6:
+            idx0 = pos + 1 if (instruction // 100) % 2 else codes[pos + 1]
+            idx1 = pos + 2 if (instruction // 1000) % 2 else codes[pos + 2]
+            if codes[idx0] == 0:
+                pos = codes[idx1]
+            else:
+                pos += 3
+        elif opcode == 7:
+            idx0 = pos + 1 if (instruction // 100) % 2 else codes[pos + 1]
+            idx1 = pos + 2 if (instruction // 1000) % 2 else codes[pos + 2]
+            idx2 = pos + 3 if (instruction // 10000) % 2 else codes[pos + 3]
+            if codes[idx0] < codes[idx1]:
+                codes[idx2] = 1
+            else:
+                codes[idx2] = 0
+            pos += 4
+        elif opcode == 8:
+            idx0 = pos + 1 if (instruction // 100) % 2 else codes[pos + 1]
+            idx1 = pos + 2 if (instruction // 1000) % 2 else codes[pos + 2]
+            idx2 = pos + 3 if (instruction // 10000) % 2 else codes[pos + 3]
+            if codes[idx0] == codes[idx1]:
+                codes[idx2] = 1
+            else:
+                codes[idx2] = 0
+            pos += 4
+        elif opcode == 99:
+            break
+        else:
+            print(f"Unknown {opcode=}")
+            breakpoint()
+    return inp
 
 
 answer = b(puzzle.input_data)
 print("b:", answer)
-assert answer == 6429
+puzzle.answer_b = answer
