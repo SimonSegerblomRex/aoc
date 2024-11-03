7f8d7b779472da7a8b68bafa6f8b3d694c2e1692
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-11-03 06:38:36 +0100

    2019/03: Add solution for part b

--- 2019/03.py
+++ 2019/03.py
@@ -85,18 +85,81 @@ for example in puzzle.examples:
     if example.answer_a:
         example_answer = a(example.input_data)
         print(f"Example answer: {example_answer} (expecting: {example.answer_a})")
-        #assert str(example_answer) == example.answer_a
+        # assert str(example_answer) == example.answer_a
 answer = a(puzzle.input_data)
 print("a:", answer)
-breakpoint()
 # assert failed for the second example... but got the correct answer for the input data..
-puzzle.answer_a = answer
-
+assert answer == 1983
 
 # Part b
-def b(data):
-    print(data)
+def intersection_b(line0, line1):
+    # Two horizontal lines
+    if (line0[0].imag == line0[1].imag) and (line1[0].imag == line1[1].imag):
+        return
+    # Two vertical lines
+    if (line0[0].real == line0[1].real) and (line1[0].real == line1[1].real):
+        return
+    # line0 horizontal
+    if line0[0].imag == line0[1].imag:
+        if (
+            min(line0[0].real, line0[1].real)
+            < line1[0].real
+            < max(line0[0].real, line0[1].real)
+        ) and (
+            min(line1[0].imag, line1[1].imag)
+            < line0[0].imag
+            < max(line1[0].imag, line1[1].imag)
+        ):
+            return line1[0].real + line0[0].imag * 1j
+        return
+    # line0 vertical
+    if line0[0].real == line0[1].real:
+        if (
+            min(line0[0].imag, line0[1].imag)
+            < line1[0].imag
+            < max(line0[0].imag, line0[1].imag)
+        ) and (
+            min(line1[0].real, line1[1].real)
+            < line0[0].real
+            < max(line1[0].real, line1[1].real)
+        ):
+            return line1[0].imag * 1j + line0[0].real
+        return
     breakpoint()
+
+
+def steps_to_intersection(lines, intersection):
+    s = 0
+    for line in lines:
+        if (intersection.imag == line[0].imag == line[1].imag) or (
+            intersection.real == line[0].real == line[1].real
+        ):
+            s += abs(intersection - line[0])
+            break
+        s += abs(line[1] - line[0])
+    return int(s)
+
+
+def b(data):
+    wire0, wire1 = data.splitlines()
+    wire0_nodes = get_nodes(wire0.split(","))
+    wire1_nodes = get_nodes(wire1.split(","))
+    wire0_segments = pairwise(wire0_nodes)
+    intersections = []
+    for segment0 in wire0_segments:
+        wire1_segments = pairwise(wire1_nodes)
+        for segment1 in wire1_segments:
+            inter = intersection_b(segment0, segment1)
+            if inter is not None:
+                intersections.append(inter)
+    wire0_segments = list(pairwise(wire0_nodes))
+    wire1_segments = list(pairwise(wire1_nodes))
+    steps = []
+    for intersection in intersections:
+        steps0 = steps_to_intersection(wire0_segments, intersection)
+        steps1 = steps_to_intersection(wire1_segments, intersection)
+        steps.append(steps0 + steps1)
+    return min(steps)
 
 
 for example in puzzle.examples:
@@ -106,4 +169,4 @@ for example in puzzle.examples:
         assert str(example_answer) == example.answer_b
 answer = b(puzzle.input_data)
 print("b:", answer)
-puzzle.answer_b = answer
+assert answer == 107754
