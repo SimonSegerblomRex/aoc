48c98aab27e7a26cc84afcf3b5eb3b6bf729a2fa
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-11-11 20:38:13 +0100

    2019/14: Slow solution for part a examples

--- 2019/14.py
+++ 2019/14.py
@@ -1,9 +1,8 @@
 import re
 from collections import defaultdict
-from fractions import Fraction
+from functools import cache
 from math import ceil
 
-import numpy as np
 from aocd.models import Puzzle
 
 YEAR = 2019
@@ -12,26 +11,46 @@ DAY = 14
 puzzle = Puzzle(year=YEAR, day=DAY)
 
 
-def calc_frac_tot_needed(reactions, needed, used):
-    for q, n in needed.items():
-        f = Fraction(n, reactions[q][0])
-        used[q] += n
-        used = calc_frac_tot_needed(reactions, {k: v * f for k, v in reactions[q][1].items()}, used.copy())
-    return used
-
-
 def calc_tot_needed(reactions, needed, used, stock):
     for q, n in needed.items():
         multiplier = ceil(n / reactions[q][0])
         multiplier = max(1, multiplier)
         stock[q] += reactions[q][0] * multiplier - n
         used[q] += n
-        #new_needed = {k: max(0, v * multiplier - stock[k]) for k, v in reactions[q][1].items()}
-        #new_needed = {k: v for k, v in reactions[q][1].items() if v > 0}
         new_needed = {k: v * multiplier for k, v in reactions[q][1].items()}
         used, stock = calc_tot_needed(reactions, new_needed, used.copy(), stock.copy())
     return used, stock
 
+
+def spend_min_ores(reactions, sstock):
+    best_score = [-1]
+
+    @cache
+    def tmp(**stock):
+        possible_to_buy = []
+        for q, (n, needed) in reactions.items():
+            for qq, nn in needed.items():
+                if qq not in stock or stock[qq] < nn:
+                    break
+            else:
+                possible_to_buy.append(q)
+        for q in possible_to_buy:
+            n, needed = reactions[q]
+            new_stock = defaultdict(int)
+            new_stock |= stock
+            new_stock[q] += n
+            for qq, nn in needed.items():
+                new_stock[qq] -= nn
+            if "FUEL" in stock:
+                if stock["ORE"] > best_score[0]:
+                    best_score[0] = stock["ORE"]
+                yield stock["ORE"]
+            if stock["ORE"] < best_score[0]:
+                yield -1
+            yield from tmp(**new_stock)
+        yield -1
+
+    return max(tmp(**sstock))
 
 
 # Part a
@@ -46,21 +65,12 @@ def a(data):
     used = defaultdict(int)
     stock = defaultdict(int)
     used, stock = calc_tot_needed(reactions, needed, used, stock)
-    # calc ore in stock
-    extra = 0
-    for k, v in stock.items():
-        if len(reactions[k][1]) == 1 and "ORE" in reactions[k][1]:
-            m = v // reactions[k][0]
-            extra -= m*reactions[k][1]["ORE"]
-    if 0:
-        while True:
-            missing = {k: ceil(used[k] / reactions[k][0]) * reactions[k][0] - used[k] for k, v in used.items()}
-            missing = {k: v for k, v in missing.items() if v > 0}
-            breakpoint()
-            if not missing:
-                break
-            used = calc_tot_needed(reactions, missing, used)
-    return used["ORE"] + extra
+    stock = {k: v for k, v in stock.items() if v > 0}
+    # used["ORE"] is an upper limit of how much ORE we might need
+    new_stock = defaultdict(int)
+    new_stock["ORE"] = used["ORE"]
+    del reactions["ORE"]
+    return used["ORE"] - spend_min_ores(reactions, new_stock)
 
 
 for example in puzzle.examples:
