ce1b58c126afbc2c3c2fb187dbf528781847258c
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-12-06 06:43:33 +0100

    2024/06: Should be faster, but still too slow

--- 2024/06.py
+++ 2024/06.py
@@ -62,27 +62,39 @@ def b(data):
             if c == "^":
                 pos = j + i*1j
     height = i + 1
-    dir = -1j
-    c = 0
+
     orig_pos = pos
-    for i in range(height):
-        for j in range(width):
-            if j + i * 1j in obstacles:
+
+
+    dir = -1j
+    visited = set()
+    while (0 <= pos.real <= width) and (0 <= pos.imag <= height):
+        visited.add(pos)
+        if pos + dir in obstacles:
+            dir *= 1j
+            continue
+        pos += dir
+
+    c = 0
+    for tmp in visited:
+        j = tmp.real
+        i = tmp.imag
+        if j + i * 1j in obstacles:
+            continue
+        obst = obstacles.copy()
+        obst.append(j + i * 1j)
+        prev_state = set()
+        pos = orig_pos
+        dir = -1j
+        while (0 <= pos.real <= width) and (0 <= pos.imag <= height):
+            if (pos, dir) in prev_state:
+                c += 1
+                break
+            prev_state.add((pos, dir))
+            if pos + dir in obst:
+                dir *= 1j
                 continue
-            obst = obstacles.copy()
-            obst.append(j + i * 1j)
-            prev_state = set()
-            pos = orig_pos
-            dir = -1j
-            while (0 <= pos.real <= width) and (0 <= pos.imag <= height):
-                if (pos, dir) in prev_state:
-                    c += 1
-                    break
-                prev_state.add((pos, dir))
-                if pos + dir in obst:
-                    dir *= 1j
-                    continue
-                pos += dir
+            pos += dir
     return c
 
 
