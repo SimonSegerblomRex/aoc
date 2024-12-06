63ec9eb77f9da08225ff8fe62a5784e9778e04d1
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-12-06 06:26:30 +0100

    2024/06: Add burte-force solution for part b
    Not finished executing yet...

--- 2024/06.py
+++ 2024/06.py
@@ -53,15 +53,42 @@ puzzle.answer_a = answer
 
 # Part b
 def b(data):
-    print(data)
-    breakpoint()
+    obstacles = []
+    for i, line in enumerate(data.splitlines()):
+        width = len(line)
+        for j, c in enumerate(line):
+            if c == "#":
+                obstacles.append(j + i*1j)
+            if c == "^":
+                pos = j + i*1j
+    height = i + 1
+    dir = -1j
+    c = 0
+    orig_pos = pos
+    for i in range(height):
+        for j in range(width):
+            if j + i * 1j in obstacles:
+                continue
+            obst = obstacles.copy()
+            obst.append(j + i * 1j)
+            prev_state = set()
+            pos = orig_pos
+            dir = -1j
+            while (0 <= pos.real <= width) and (0 <= pos.imag <= height):
+                if (pos, dir) in prev_state:
+                    c += 1
+                    break
+                prev_state.add((pos, dir))
+                if pos + dir in obst:
+                    dir *= 1j
+                    continue
+                pos += dir
+    return c
 
 
-for example in puzzle.examples:
-    if example.answer_b:
-        example_answer = b(example.input_data)
-        print(f"Example answer: {example_answer} (expecting: {example.answer_b})")
-        assert str(example_answer) == example.answer_b
+answer = b(example)
+print(answer)
+assert answer == 6
 answer = b(puzzle.input_data)
 print("b:", answer)
 puzzle.answer_b = answer
