75257e4cdcbcdb31ec4e535ca4920865bdcbf876
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-12-07 07:23:09 +0100

    2024/07: Add solution for part b

--- 2024/07.py
+++ 2024/07.py
@@ -1,11 +1,7 @@
-import datetime
-import re
-
-import numpy as np
 from aocd.models import Puzzle
 
-YEAR = datetime.datetime.today().year
-DAY = datetime.datetime.today().day
+YEAR = 2024
+DAY = 7
 
 puzzle = Puzzle(year=YEAR, day=DAY)
 
@@ -38,15 +34,15 @@ for example in puzzle.examples:
         assert str(example_answer) == example.answer_a
 answer = a(puzzle.input_data)
 print("a:", answer)
-puzzle.answer_a = answer
+assert answer == 3119088655389
 
 
 def evaluate_b(numbers, current, answer):
     if len(numbers) == 1:
         return current == answer
-    if (evaluate(numbers[1:], current + numbers[1], answer)
-        or evaluate(numbers[1:], current * numbers[1], answer)
-        or evaluate(numbers[1:], int(str(current) + str(numbers[1])), answer)):
+    if (evaluate_b(numbers[1:], current + numbers[1], answer)
+        or evaluate_b(numbers[1:], current * numbers[1], answer)
+        or evaluate_b(numbers[1:], int(str(current) + str(numbers[1])), answer)):
         return True
     return False
 
@@ -70,4 +66,4 @@ for example in puzzle.examples:
         assert str(example_answer) == example.answer_b
 answer = b(puzzle.input_data)
 print("b:", answer)
-puzzle.answer_b = answer
+assert answer == 264184041398847
