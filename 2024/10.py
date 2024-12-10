ff7d3ca3e78b3ad6638a96bfa5d4f17ff168b4c2
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-12-10 06:30:46 +0100

    2024/10: Add solution for part b

--- 2024/10.py
+++ 2024/10.py
@@ -1,12 +1,10 @@
-import datetime
-import re
 from collections import defaultdict
 
 import numpy as np
 from aocd.models import Puzzle
 
-YEAR = datetime.datetime.today().year
-DAY = datetime.datetime.today().day
+YEAR = 2024
+DAY = 10
 
 puzzle = Puzzle(year=YEAR, day=DAY)
 
@@ -49,23 +47,41 @@ example = """89010123
 01329801
 10456732"""
 answer = a(example)
-breakpoint()
+assert answer == 36
 answer = a(puzzle.input_data)
 print("a:", answer)
-puzzle.answer_a = answer
+assert answer == 514
 
 
 # Part b
 def b(data):
-    print(data)
-    breakpoint()
+    points = defaultdict(list)
+    grid = {}
+    for i, line in enumerate(data.splitlines()):
+        width = len(line)
+        for j, c in enumerate(line):
+            points[c].append(j + i*1j)
+            grid[j + i*1j] = int(c)
+    s = 0
+    def neighbours(p):
+        return p + 1, p - 1j, p - 1, p + 1j
+    for pos in points["0"]:
+        curr = [pos]
+        while curr:
+            new = []
+            for p in curr:
+                if grid[p] == 9:
+                    s += 1
+                neigh = neighbours(p)
+                neigh = [n for n in neigh if n in grid]
+                neigh = [n for n in neigh if grid[n] - 1 == grid[p]]
+                new.extend(neigh)
+            curr = new
+    return s
 
 
-for example in puzzle.examples:
-    if example.answer_b:
-        example_answer = b(example.input_data)
-        print(f"Example answer: {example_answer} (expecting: {example.answer_b})")
-        assert str(example_answer) == example.answer_b
+answer = b(example)
+assert answer == 81
 answer = b(puzzle.input_data)
 print("b:", answer)
-puzzle.answer_b = answer
+assert answer == 1162
