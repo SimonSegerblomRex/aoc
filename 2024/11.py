e56ee05291eefb7afdcd3b7fdc0ce9e3ebe0fc0d
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-12-11 06:26:09 +0100

    2024/11: First speedup

--- 2024/11.py
+++ 2024/11.py
@@ -11,23 +11,51 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 
 example = "0 1 10 99 999"
 
+
+def blink(stones):
+    new = []
+    for s in stones:
+        ss = str(s)
+        if s == 0:
+            new.append(1)
+        elif not len(ss) % 2:
+            w = len(ss)
+            new.append(int(ss[:w//2]))
+            new.append(int(ss[w//2:]))
+        else:
+            new.append(2024 * s)
+    return new
+
+
+def blink2(stones, magic):
+    new = []
+    extra = 0
+    for s in stones:
+        ss = str(s)
+        if s == 0:
+            extra += magic
+        elif not len(ss) % 2:
+            w = len(ss)
+            new.append(int(ss[:w//2]))
+            new.append(int(ss[w//2:]))
+        else:
+            new.append(2024 * s)
+    return new, extra
+
+
 # Part a
 def a(data, blinks=25):
-    stones = list(map(int, data.split()))
+    stones = [0]
+    zero = []
     for _ in range(blinks):
-        new = []
-        for s in stones:
-            ss = str(s)
-            if s == 0:
-                new.append(1)
-            elif not len(ss) % 2:
-                w = len(ss)
-                new.append(int(ss[:w//2]))
-                new.append(int(ss[w//2:]))
-            else:
-                new.append(2024 * s)
-        stones = new
-    return len(stones)
+        stones = blink(stones)
+        zero.append(len(stones))
+    stones = list(map(int, data.split()))
+    extra = 0
+    for i, _ in enumerate(range(blinks)):
+        stones, plus = blink2(stones, zero[blinks - 1 - i])
+        extra += plus
+    return len(stones) + extra
 
 
 example = "125 17"
