07b02d77bd51dcfd9e377fa59aff2e400f748891
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-12-11 10:01:42 +0100

    2024/11: Kladd

--- 2024/11.py
+++ 2024/11.py
@@ -1,5 +1,7 @@
 import datetime
 import re
+from collections import Counter
+from functools import cache
 
 import numpy as np
 from aocd.models import Puzzle
@@ -12,50 +14,44 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 example = "0 1 10 99 999"
 
 
-def blink(stones):
-    new = []
-    for s in stones:
-        ss = str(s)
-        if s == 0:
-            new.append(1)
-        elif not len(ss) % 2:
-            w = len(ss)
-            new.append(int(ss[:w//2]))
-            new.append(int(ss[w//2:]))
-        else:
-            new.append(2024 * s)
-    return new
+def blinkhelper(stone):
+    ss = str(stone)
+    if stone == 0:
+        return [1]
+    elif not len(ss) % 2:
+        w = len(ss)
+        return [int(ss[:w//2]), int(ss[w//2:])]
+    return [2024 * stone]
 
 
-def blink2(stones, magic):
+@cache
+def blinkk(stones, blinks):
+    if blinks == 0:
+        return stones
     new = []
-    extra = 0
     for s in stones:
-        ss = str(s)
-        if s == 0:
-            extra += magic
-        elif not len(ss) % 2:
-            w = len(ss)
-            new.append(int(ss[:w//2]))
-            new.append(int(ss[w//2:]))
-        else:
-            new.append(2024 * s)
-    return new, extra
+        new += blinkhelper(s)
+    new = sorted(new)
+    return blinkk(tuple(new), blinks - 1)
+
+
+@cache
+def blink(stone, blinks):
+    return blinkk((stone,), blinks)
 
 
 # Part a
 def a(data, blinks=25):
-    stones = [0]
-    zero = []
-    for _ in range(blinks):
-        stones = blink(stones)
-        zero.append(len(stones))
     stones = list(map(int, data.split()))
-    extra = 0
-    for i, _ in enumerate(range(blinks)):
-        stones, plus = blink2(stones, zero[blinks - 1 - i])
-        extra += plus
-    return len(stones) + extra
+    chunk = 25
+    tot = 0
+    for _ in range(blinks // chunk):
+        stones = Counter(stones)
+        new = []
+        for s, c in stones.items():
+            new += c * blink(s, chunk)
+        stones = new
+    return sum(Counter(stones).values())
 
 
 example = "125 17"
