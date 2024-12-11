3900e492ff6e37c6ab0e23be95b1d0c158e15410
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-12-11 19:15:19 +0100

    2024/11: Minor cleanup

--- 2024/11.py
+++ 2024/11.py
@@ -9,27 +9,30 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 
 
 def blink_helper(stone):
-    ss = str(stone)
     if stone == 0:
         return [1]
-    elif not len(ss) % 2:
-        w = len(ss)
-        return [int(ss[: w // 2]), int(ss[w // 2 :])]
+    stone_str = str(stone)
+    nbr_digits = len(stone_str)
+    if not nbr_digits % 2:
+        return [
+            int(stone_str[: nbr_digits // 2]),
+            int(stone_str[nbr_digits // 2 :]),
+        ]
     return [2024 * stone]
 
 
 def blink(stones):
     out = defaultdict(int)
     for stone, count in stones.items():
-        new = blink_helper(stone)
-        for n in new:
-            out[n] += count
+        new_stones = blink_helper(stone)
+        for new_stone in new_stones:
+            out[new_stone] += count
     return out
 
 
 # Part a
 def a(data, blinks=25):
-    stones = list(map(int, data.split()))
+    stones = [int(n) for n in data.split()]
     stones = Counter(stones)
     for _ in range(blinks):
         stones = blink(stones)
