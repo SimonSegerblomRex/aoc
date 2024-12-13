048a713ede38927c88c7fcc0a80247956efb0244
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-12-13 07:06:57 +0100

    2024/13: Minor cleanup

--- 2024/13.py
+++ 2024/13.py
@@ -1,12 +1,9 @@
-import datetime
 import re
-from scipy.linalg import inv
 
-import numpy as np
 from aocd.models import Puzzle
 
-YEAR = datetime.datetime.today().year
-DAY = datetime.datetime.today().day
+YEAR = 2024
+DAY = 13
 
 puzzle = Puzzle(year=YEAR, day=DAY)
 
@@ -23,7 +20,6 @@ def a(data, extra=0):
     machines = [[int(n) for n in m] for m in machines]
     s = 0
     for m in machines:
-        #tmp = np.linalg.inv(np.array([[m[0], m[2]], [m[1], m[3]]], dtype=int).reshape(2, 2))
         x0, x1, x2, x3 =  m[0], m[2], m[1], m[3]
         det = x0*x3 - x1*x2
         m[-1] += extra
@@ -36,16 +32,12 @@ def a(data, extra=0):
     return s
 
 
-answer = a(puzzle.examples[0].input_data)
-print(answer)
-assert answer == 480
 answer = a(puzzle.input_data)
 print("a:", answer)
-puzzle.answer_a = answer
-breakpoint()
+assert answer == 36954
 
 
 # Part b
 answer = a(puzzle.input_data, extra=10000000000000)
 print("b:", answer)
-puzzle.answer_b = answer
+assert answer == 79352015273424
