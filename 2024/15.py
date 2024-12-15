5a5321c298cf99c6c3735fa742d46319710ba879
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-12-15 08:35:03 +0100

    2024/15: Kladd b

--- 2024/15.py
+++ 2024/15.py
@@ -69,15 +69,161 @@ puzzle.answer_a = answer
 
 # Part b
 def b(data):
-    print(data)
-    breakpoint()
+    grid, moves = data.split("\n\n")
+    grid = grid.replace("#", "##")
+    grid = grid.replace("O", "[]")
+    grid = grid.replace(".", "..")
+    grid = grid.replace("@", "@.")
+    wall = set()
+    boxes = set()
+    box_coords = set()
+    pos = None
+    for i, line in enumerate(grid.splitlines()):
+        width = len(line)
+        for j, c in enumerate(line):
+            if c == "#":
+                wall.add(j + i*1j)
+                wall.add(j + 0.5 + i*1j)
+            elif c == "[":
+                boxes.add(j + i*1j)
+                box_coords.add(j + 0.5 + i*1j)
+            elif c == "]":
+                boxes.add(j + i*1j)
+            elif c == "@":
+                pos = j + i*1j
+    height = i + 1
+    move2dir = {
+        ">": 1,
+        "^": -1j,
+        "<": -1,
+        "v": 1j,
+    }
+    for move in moves.replace("\n", ""):
+        boxes = set()
+        for box_coord in box_coords:
+            boxes.add(box_coord - 0.5)
+            boxes.add(box_coord + 0.5)
+        if 1:
+            for i in range(height):
+                for j in range(width):
+                    p = j + i*1j
+                    if p in wall:
+                        if int(p.real) == p.real:
+                            print("#", end="")
+                    elif p == pos:
+                        print("@", end="")
+                    elif p - 0.5 in box_coords:
+                        print("[]", end="")
+                    elif p + 0.5 in box_coords:
+                        pass
+                    else:
+                        print(".", end="")
+                print()
+            print(move)
+            breakpoint()
+        dir = move2dir[move]
+        if pos + dir in wall:
+            continue
+        if pos + dir not in boxes:
+            pos += dir
+            continue
+        boxes_to_move = 0
+        pos_to_check = pos + dir
+        while True:
+            if pos_to_check in boxes:
+                boxes_to_move += 1
+                pos_to_check += dir
+            elif pos_to_check in wall:
+                boxes_to_move = 0
+                break
+            else:
+                break
+        if boxes_to_move > 0:
+            if dir in (1, -1):
+                box_coord_to_move = pos + dir * 1.5
+                pos += dir
+                for i in range(boxes_to_move // 2):
+                    if box_coord_to_move not in box_coords:
+                        break
+                    box_coords.remove(box_coord_to_move)
+                    box_coords.add(box_coord_to_move + dir)
+                    box_coord_to_move += 2*dir
+                continue
+            boxes_to_move = set()
+            pot_boxes = set([pos + dir - 0.5, pos + dir + 0.5])
+            box = (pot_boxes & box_coords).pop()
+            boxes_to_move.add(box)
+            can_move = True
+            while True:
+                tmp = boxes_to_move.copy()
+                for box in tmp:
+                    cand0 = box + dir - 1
+                    cand1 = box + dir + 1
+                    cand2 = box + dir
+                    if cand0 in box_coords:
+                        boxes_to_move.add(cand0)
+                    elif cand0 in wall:
+                        can_move = False
+                        break
+                    if cand1 in box_coords:
+                        boxes_to_move.add(cand1)
+                    elif cand1 in wall:
+                        can_move = False
+                        break
+                    if cand2 in box_coords:
+                        boxes_to_move.add(cand2)
+                    elif cand2 in wall:
+                        can_move = False
+                        break
+                if tmp == boxes_to_move:
+                    break
+            if can_move:
+                pos += dir
+                new_box_coords = set()
+                for box in boxes_to_move:
+                    new_box_coords.add(box + dir)
+                    box_coords.remove(box)
+                box_coords |= new_box_coords
+    return sum(box.real - 0.5 + 100 * box.imag for box in box_coords)
 
 
-for example in puzzle.examples:
-    if example.answer_b:
-        example_answer = b(example.input_data)
-        print(f"Example answer: {example_answer} (expecting: {example.answer_b})")
-        assert str(example_answer) == example.answer_b
+example0 = """#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^"""
+
+example = """##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"""
+if 0:
+    answer = b(example0)
+    print("b:", answer)
+answer = b(example)
+print("b:", answer)
+assert answer == 9021
 answer = b(puzzle.input_data)
 print("b:", answer)
 puzzle.answer_b = answer
