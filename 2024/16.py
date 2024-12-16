d0deedf1501ddc17b2e97c61b1fcd3c57eb22419
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-12-16 07:20:47 +0100

    2024/16: Add solution for examples...

--- 2024/16.py
+++ 2024/16.py
@@ -77,17 +77,112 @@ print("a:", answer)
 puzzle.answer_a = answer
 
 
+def a_star_modified(start, goal, walls):
+    def h(node):
+        return int(abs(goal.real - node.real) + abs(goal.imag - node.imag))
+
+    open_set = queue.PriorityQueue()
+    # (f_score, dummy, pos, dir, path)
+    open_set.put((0, 0, start, 1, [start]))
+
+    g_score = defaultdict(lambda: 1 << 30)
+    g_score[(start, 1)] = 0
+
+    f_score = defaultdict(lambda: 1 << 30)
+    f_score[(start, 1)] = h(start)
+
+    i = 1
+    good = set()
+    best_score = 1 << 30
+    while not open_set.empty():
+        _, _, curr_pos, curr_dir, path = open_set.get()
+        if curr_pos == goal:
+            if int(g_score[(curr_pos, curr_dir)]) < best_score:
+                best_score = int(g_score[(curr_pos, curr_dir)])
+            if int(g_score[(curr_pos, curr_dir)]) <= best_score:
+                good |= set(path)
+
+        dirs = [1, -1j, -1, 1j]
+        for dir in dirs:
+            turning_cost = 0
+            if dir != curr_dir:
+                turning_cost = 1000
+            if dir == -curr_dir:
+                continue
+            next_node = curr_pos + dir
+            if next_node in walls:
+                continue
+            tentative_g_score = g_score[(curr_pos, curr_dir)] + 1 + turning_cost
+            if tentative_g_score <= g_score[(next_node, dir)]:
+                g_score[(next_node, dir)] = tentative_g_score
+                f_score[(next_node, dir)] = tentative_g_score + h(next_node)
+                open_set.put(
+                    (
+                        f_score[(next_node, dir)],
+                        i,
+                        next_node,
+                        dir,
+                        [*path, next_node]
+                    )
+                )
+                i += 1
+
+    if 0:
+        height = int(max(n.imag for n in walls)) + 1
+        width = int(max(n.real for n in walls)) + 1
+        for i in range(height):
+            for j in range(width):
+                p =  j + i * 1j
+                if p in walls:
+                    print("#", end="")
+                elif p in good:
+                    print("O", end="")
+                else:
+                    print(".", end="")
+            print()
+
+    return len(good)
+
+
 # Part b
 def b(data):
-    print(data)
-    breakpoint()
+    walls = set()
+    for i, line in enumerate(data.splitlines()):
+        for j, c in enumerate(line):
+            if c == "#":
+                walls.add(j + i*1j)
+            elif c == "S":
+                start = j + i*1j
+            elif c == "E":
+                goal = j + i*1j
+
+    return a_star_modified(start, goal, walls)
 
 
-for example in puzzle.examples:
-    if example.answer_b:
-        example_answer = b(example.input_data)
-        print(f"Example answer: {example_answer} (expecting: {example.answer_b})")
-        assert str(example_answer) == example.answer_b
+answer = b(puzzle.examples[0].input_data)
+print("b:", answer)
+assert answer == 45
+example = """#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################"""
+answer = b(example)
+print("b:", answer)
+assert answer == 64
 answer = b(puzzle.input_data)
 print("b:", answer)
+assert answer < 700
 puzzle.answer_b = answer
