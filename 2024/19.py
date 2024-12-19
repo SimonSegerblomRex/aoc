ccef3e76167037426c76cbb59e10130c4e59d7d8
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-12-19 06:33:15 +0100

    2024/19: Add messy solution for part b

--- 2024/19.py
+++ 2024/19.py
@@ -47,15 +47,45 @@ puzzle.answer_a = answer
 
 # Part b
 def b(data):
-    print(data)
-    breakpoint()
+    patterns, designs = data.split("\n\n")
+    patterns = patterns.split(", ")
+    designs = designs.split()
+    @cache
+    def check(design):
+        if not design or design in patterns:
+            return True
+        ok = False
+        for i in range(len(design)):
+            if design[:i] in patterns:
+                ok = check(design[i:])
+                if ok:
+                    break
+        return ok
+    @cache
+    def count(design):
+        if len(design) == 1 and design in patterns:
+            return 1
+        if not design:
+            return 0
+        c = 0
+        if design in patterns:
+            c += 1
+        for i in range(len(design)):
+            if design[:i] in patterns:
+                if check(design[i:]):
+                    c += count(design[i:])
+        return c
+    s = 0
+    for design in designs:
+        s += count(design)
+    return s
 
 
 for example in puzzle.examples:
     if example.answer_b:
         example_answer = b(example.input_data)
-        print(f"Example answer: {example_answer} (expecting: {example.answer_b})")
-        assert str(example_answer) == example.answer_b
+        print(f"Example answer: {example_answer} (expecting: {16})")
+        assert example_answer == 16
 answer = b(puzzle.input_data)
 print("b:", answer)
 puzzle.answer_b = answer
