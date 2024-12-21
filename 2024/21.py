57dede9f2df3d7de2f16d3b2cb9fd3574ecfa38f
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-12-21 17:31:19 +0100

    2024/21: Cleanup

--- 2024/21.py
+++ 2024/21.py
@@ -82,29 +82,33 @@ def path_directional(start, goal):
     return path
 
 
+@cache
+def count(path, i, pos="A"):
+    c = 0
+    for d in path:
+        path = path_directional(pos, d)
+        pos = d
+        path += "A"
+        if i > 0:
+            c += count(path, i - 1)
+        else:
+            c += len(path)
+    return c
+
+
 # Part a
-def a(data):
+def a(data, drobots=2):
     codes = data.split()
     s = 0
-    pos_d1 = "A"
-    pos_dn = "A"
     pos_n = 10
     for code_str in codes:
         code = [int(n, 16) for n in code_str]
         c = 0
-        for target in code:
-            path_n = path_numeric(pos_n, target)
-            pos_n = target
-            path_n += "A"
-            for d in path_n:
-                path_d = path_directional(pos_dn, d)
-                pos_dn = d
-                path_d += "A"
-                for d in path_d:
-                    path_d = path_directional(pos_d1, d)
-                    pos_d1 = d
-                    path_d += "A"
-                    c += len(path_d)
+        for d in code:
+            path = path_numeric(pos_n, d)
+            pos_n = d
+            path += "A"
+            c += count(path, drobots - 1)
         s += int(code_str[:3]) * c
     return s
 
@@ -122,78 +126,6 @@ print("a:", answer)
 assert answer == 162740
 
 # Part b
-def btmp(data):
-    codes = data.split()
-    s = 0
-    pos_d1 = "A"
-    pos_d2 = "A"
-    pos_d3 = "A"
-    pos_d4 = "A"
-    pos_n = 10
-    for code_str in codes:
-        code = [int(n, 16) for n in code_str]
-        c = 0
-        for d in code:
-            path_d = path_numeric(pos_n, d)
-            pos_n = d
-            path_d += "A"
-            for d in path_d:
-                path_d = path_directional(pos_d4, d)
-                pos_d4 = d
-                path_d += "A"
-                for d in path_d:
-                    path_d = path_directional(pos_d3, d)
-                    pos_d3 = d
-                    path_d += "A"
-                    for d in path_d:
-                        path_d = path_directional(pos_d2, d)
-                        pos_d2 = d
-                        path_d += "A"
-                        for d in path_d:
-                            path_d = path_directional(pos_d1, d)
-                            pos_d1 = d
-                            path_d += "A"
-                            c += len(path_d)
-        s += int(code_str[:3]) * c
-    return s
-
-
-def bb(data, drobots=25):
-    codes = data.split()
-    s = 0
-    pos_n = 10
-    for code_str in codes:
-        code = [int(n, 16) for n in code_str]
-        c = 0
-        for d in code:
-            path = path_numeric(pos_n, d)
-            pos_n = d
-            path += "A"
-            @cache
-            def count(path, i, pos="A"):
-                c = 0
-                for d in path:
-                    path = path_directional(pos, d)
-                    pos = d
-                    path += "A"
-                    if i > 0:
-                        c += count(path, i - 1)
-                    else:
-                        c += len(path)
-                return c
-            c += count(path, drobots - 1)
-        s += int(code_str[:3]) * c
-    return s
-
-
-answer_b = btmp(puzzle.input_data)
-print("btmp:", answer)
-answer_bb = bb(puzzle.input_data, 25)
-print("bb:", answer_bb)
-assert btmp(puzzle.input_data) == bb(puzzle.input_data, 4)
-assert a(puzzle.input_data) == bb(puzzle.input_data, 2)
-assert 128183088638138 < answer_bb < 325407610843116
-#assert answer_bb != 328240366912228
-assert answer_bb != 241523801340054
-assert answer_bb != 175407510093318
-puzzle.answer_b = answer_bb
+answer = a(puzzle.input_data, 25)
+print("b:", answer)
+assert answer == 203640915832208
