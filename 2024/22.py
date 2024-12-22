9297ec610966d4451c8fea272ff453471b0f1b9f
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-12-22 07:07:29 +0100

    2024/22: Add brute-force solution for part b
    (Might be too slow.)

--- 2024/22.py
+++ 2024/22.py
@@ -1,5 +1,6 @@
 import datetime
 import re
+from itertools import product
 
 import numpy as np
 from aocd.models import Puzzle
@@ -39,14 +40,52 @@ assert answer == 12664695565
 
 # Part b
 def b(data):
-    breakpoint()
+    #numbers = []
+    prices = []
+    diff = []
+    for n in data.split():
+        n = int(n)
+        tmp_n = [n]
+        tmp_p = [n % 10]
+        tmp_d = [0]
+        for _ in range(2000):
+            n = int(n)
+            n ^= 64 * n
+            n %= 16777216
+            n ^= n // 32
+            n %= 16777216
+            n ^= 2048 * n
+            n %= 16777216
+            tmp_d.append(n % 10 - tmp_p[-1])
+            tmp_p.append(n % 10)
+            tmp_n.append(n)
+        #numbers.append(tmp_n)
+        prices.append(tmp_p)
+        diff.append(tmp_d)
+    seqs = set()
+    for p, d in zip(prices, diff):
+        for i in range(1, len(p) - 3):
+            seqs.add(tuple(d[i:i + 4]))
+    bananas = {}
+    #for seq in product(range(-9, 9 + 1), repeat=4):
+    for seq in seqs:
+        s = 0
+        for p, d in zip(prices, diff):
+            for i in range(1, len(p) - 3):
+                if tuple(d[i:i + 4]) == seq:
+                    s += p[i + 3]
+                    break
+        bananas[seq] = s
+    return max(bananas.values())
 
 
-for example in puzzle.examples:
-    if example.answer_b:
-        example_answer = b(example.input_data)
-        print(f"Example answer: {example_answer} (expecting: {example.answer_b})")
-        assert str(example_answer) == example.answer_b
+example = """1
+2
+3
+2024"""
+answer = b(example)
+print(answer)
+assert answer == 23
 answer = b(puzzle.input_data)
 print("b:", answer)
 puzzle.answer_b = answer
