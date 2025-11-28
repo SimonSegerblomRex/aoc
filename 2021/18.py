ee9e3990d185e466d7610cf071ceba850cf91ece
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2025-11-28 21:39:54 +0100

    2021/18: Minor cleanup

--- 2021/18.py
+++ 2021/18.py
@@ -28,50 +28,47 @@ class SnailFishNumber:
             raise ValueError("Can't handle val...")
 
     def __repr__(self):
-        if self.left is None and self.right is None:
+        if self.val is not None:
             return str(self.val)
         return f"[{self.left}, {self.right}]"
 
-    def leaves(self, l=None):
-        if l is None:
-            l = []
-        if self.left is None and self.right is None:
-            l.append(self)
-            return l
-        l.extend(e for e in self.left.leaves(l) if e not in l)
-        l.extend(e for e in self.right.leaves(l) if e not in l)
+    def leaves(self):
+        if self.val is not None:
+            return [self]
+        l = self.left.leaves()
+        l += self.right.leaves()
         return l
+
+    def reduce(self):
+        l = self.leaves()
+        for i, e in enumerate(l):
+            if e.depth == 5:
+                # explode
+                assert l[i + 1].val is not None
+                if i > 0:
+                    l[i - 1].val += e.val
+                try:
+                    l[i + 2].val += l[i + 1].val
+                except IndexError:
+                    pass
+                e.parent.left = None
+                e.parent.right = None
+                e.parent.val = 0
+                self.reduce()
+                return
+        for e in l:
+            if e.val >= 10:
+                # split
+                e.left = SnailFishNumber(e.val // 2, e.depth + 1, e)
+                e.right = SnailFishNumber((e.val + 1) // 2, e.depth + 1, e)
+                e.val = None
+                self.reduce()
+                return
 
     def magnitude(self):
         if self.val is not None:
             return self.val
         return 3 * self.left.magnitude() + 2 * self.right.magnitude()
-
-
-def reduce(n):
-    l = n.leaves()
-    for i, e in enumerate(l):
-        if e.depth == 5:
-            # explode
-            assert l[i + 1].val is not None
-            if i > 0:
-                l[i - 1].val += e.val
-            try:
-                l[i + 2].val += l[i + 1].val
-            except IndexError:
-                pass
-            e.parent.left = None
-            e.parent.right = None
-            e.parent.val = 0
-            return True
-    for i, e in enumerate(l):
-        if e.val >= 10:
-            # split
-            e.left = SnailFishNumber(e.val // 2, e.depth + 1, e)
-            e.right = SnailFishNumber((e.val + 1) // 2, e.depth + 1, e)
-            e.val = None
-            return True
-    return False
 
 
 # Part a
@@ -84,8 +81,7 @@ def a(data):
         l1 = ast.literal_eval(str(prev))
         l2 = ast.literal_eval(str(n))
         prev = SnailFishNumber([l1, l2])
-        while reduce(prev):
-            pass
+        prev.reduce()
     return prev.magnitude()
 
 
@@ -106,8 +102,7 @@ def b(data):
         l1 = ast.literal_eval(n1)
         l2 = ast.literal_eval(n2)
         prev = SnailFishNumber([l1, l2])
-        while reduce(prev):
-            pass
+        prev.reduce()
         high_score = max(prev.magnitude(), high_score)
     return high_score
 
