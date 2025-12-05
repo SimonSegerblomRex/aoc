b212e334a91a0df038fe967d0ddcd6e26a78ec6d
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2025-12-05 07:22:04 +0100

    2025/05: Minor cleanup (but b still broken...)

--- 2025/05.py
+++ 2025/05.py
@@ -9,13 +9,13 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 # Part a
 def a(data):
     ranges, ids = data.split("\n\n")
-    rr = []
+    all_ranges = set()
     for r in ranges.split():
         lo, hi = r.split("-")
-        rr.append(range(int(lo), int(hi) + 1))
+        all_ranges.add(range(int(lo), int(hi) + 1))
     c = 0
     for id in ids.split():
-        for r in rr:
+        for r in all_ranges:
             if int(id) in r:
                 c += 1
                 break
@@ -34,26 +34,27 @@ assert answer == 698
 # Part b
 def b(data):
     ranges, ids = data.split("\n\n")
-    rr = []
+    all_ranges = set()
     for r in ranges.split():
         lo, hi = r.split("-")
-        rr.append(range(int(lo), int(hi) + 1))
-    gg = set()
+        all_ranges.add(range(int(lo), int(hi) + 1))
+    fresh_ranges = set()
     for id in ids.split():
-        for r in rr:
+        for r in all_ranges:
             if int(id) in r:
-                gg.add(r)
-    gg = sorted(list(gg), key=lambda r: (r.start, r.stop))
-    gg = [[g.start, g.stop - 1] for g in gg]
-    oo = [gg[0].copy()]
-    for g in gg[1:]:
-        if g[1] > oo[-1][1]:
-            if g[0] <= oo[-1][1]:
-                oo[-1][1] = g[1]
+                fresh_ranges.add(r)
+    fresh_ranges = sorted(list(fresh_ranges), key=lambda r: (r.start, r.stop))
+    fresh_ranges = [[r.start, r.stop - 1] for r in fresh_ranges]
+    union = [fresh_ranges[0]]
+    for g in fresh_ranges[1:]:
+        if g[1] > union[-1][1]:
+            if g[0] <= union[-1][1]:
+                union[-1][1] = g[1]
             else:
-                oo.append(g.copy())
+                union.append(g)
     s = 0
-    for g in oo:
+    for g in union:
+        assert g[1] > g[0]
         s += g[1] - g[0] + 1
     return s
 
@@ -67,4 +68,4 @@ assert answer != 337646673718690
 assert answer != 337878930639144
 assert answer != 337878930639150
 assert answer != 348324210241501
-#puzzle.answer_b = answer
+puzzle.answer_b = answer
