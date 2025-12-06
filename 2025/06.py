894468b131b18afded8efb2a144e1aa09a6040ed
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2025-12-06 11:16:25 +0100

    2025/06: Cleanup

--- 2025/06.py
+++ 2025/06.py
@@ -1,35 +1,21 @@
-import datetime
 import re
 
-import numpy as np
 from aocd.models import Puzzle
 
-YEAR = datetime.datetime.today().year
-DAY = datetime.datetime.today().day
+YEAR = 2025
+DAY = 6
 
 puzzle = Puzzle(year=YEAR, day=DAY)
 
 
 # Part a
 def a(data):
-    problems = []
-    for j, line in enumerate(data.split("\n")):
-        for i, e in enumerate([e for e in line.split(" ") if e]):
-            if i > len(problems) - 1:
-                problems.append([])
-            problems[i].append(e)
+    numbers = re.findall(r"\d+", data)
+    operators = data.splitlines()[-1].split()
+    nbr_problems = len(operators)
     s = 0
-    for problem in problems:
-        if problem[-1] == "+":
-            tmp = 0
-        else:
-            tmp = 1
-        for e in problem[:-1]:
-            if problem[-1] == "+":
-                tmp += int(e)
-            else:
-                tmp *= int(e)
-        s += tmp
+    for i, operator in enumerate(operators):
+        s += eval(f"{operator}".join(numbers[i::nbr_problems]))
     return s
 
 
@@ -45,37 +31,14 @@ assert answer == 5335495999141
 
 # Part b
 def b(data):
-    chars = {}
-    max_i = 0
-    for j, line in enumerate(data.split("\n")):
-        for i, c in enumerate(line):
-            chars[(j, i)] = c
-        max_i = max(i, max_i)
-    max_j = j
-    start_idx = []
-    for i, c in enumerate(line):
-        if c in "+*":
-            start_idx.append(i)
-    stop_idx = [e - 2 for e in start_idx[1:]]
-    stop_idx.append(max_i)
+    *data, operators = data.splitlines()
+    numbers = ["".join(line[i] for line in data).strip() for i in range(len(data[0]))]
+    start_idx = [i for i, operator in enumerate(operators) if operator != " "]
+    stop_idx = [idx - 1 for idx in start_idx[1:]]
+    stop_idx.append(len(operators))
     s = 0
-    for start, stop in zip(start_idx, stop_idx):
-        numbers = []
-        for i in range(start, stop + 1):
-            numbers.append([])
-            for j in range(max_j):
-                numbers[i - start].append(chars[(j, i)])
-        numbers = [int("".join(e)) for e in numbers]
-        if line[start] == "+":
-            tmp = 0
-        else:
-            tmp = 1
-        for e in numbers:
-            if line[start] == "+":
-                tmp += int(e)
-            else:
-                tmp *= int(e)
-        s += tmp
+    for start, stop, operator in zip(start_idx, stop_idx, operators.split()):
+        s += eval(f"{operator}".join(numbers[start:stop]))
     return s
 
 
