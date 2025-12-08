1fb942ab01eda6862979eb42b0733be8e9f4aea2
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2025-12-08 10:31:23 +0100

    2025/08: Add solution for part b

--- 2025/08.py
+++ 2025/08.py
@@ -78,7 +78,48 @@ assert answer == 66912
 
 # Part b
 def b(data):
-    breakpoint()
+    coords = set()
+    for line in data.splitlines():
+        coords.add(tuple(map(int, line.split(","))))
+    distances = {}
+    for c0 in coords:
+        for c1 in coords:
+            if c0 == c1:
+                continue
+            #distances[tuple(sorted((c0, c1)))] = distance(*c0, *c1)
+            if (c1, c0) not in distances:
+                distances[(c0, c1)] = distance(*c0, *c1)
+    distances = dict(sorted(distances.items(), key=lambda t: t[1]))
+    circuits = []
+    circuit_map = {}
+    for c0, c1 in distances:
+        if c0 in circuit_map and c1 in circuit_map:
+            if circuit_map[c0] == circuit_map[c1]:
+                # Do nothing
+                continue
+            # Combine
+            circuit = circuit_map[c1]
+            circuits.remove(circuit)
+            for c in circuit:
+                circuit_map[c] = circuit_map[c0]
+                circuit_map[c0].add(c)
+        elif c0 in circuit_map:
+            # c1 new
+            assert c1 not in circuit_map
+            circuit_map[c0].add(c1)
+            circuit_map[c1] = circuit_map[c0]
+        elif c1 in circuit_map:
+            # c0 new
+            assert c0 not in circuit_map
+            circuit_map[c1].add(c0)
+            circuit_map[c0] = circuit_map[c1]
+        else:
+            circuit = set((c0, c1))
+            circuits.append(circuit)
+            circuit_map[c0] = circuit
+            circuit_map[c1] = circuit
+        if len(circuits[0]) == len(coords):
+            return c0[0] * c1[0]
 
 
 for example in puzzle.examples:
