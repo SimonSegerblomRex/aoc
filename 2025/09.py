ab90ec32973b1b6cf63f305d0ccdaea14d415ac5
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2025-12-09 07:54:50 +0100

    2025/09: Minor cleanup

--- 2025/09.py
+++ 2025/09.py
@@ -1,13 +1,10 @@
-import datetime
-
-import matplotlib.pyplot as plt
 from shapely import Point, Polygon
 from shapely.plotting import plot_polygon
 
 from aocd.models import Puzzle
 
-YEAR = datetime.datetime.today().year
-DAY = datetime.datetime.today().day
+YEAR = 2025
+DAY = 9
 
 puzzle = Puzzle(year=YEAR, day=DAY)
 
@@ -15,6 +12,7 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 # Part a
 def area(x0, y0, x1, y1):
     return (abs(x1 - x0) + 1) * (abs(y1 - y0) + 1)
+
 
 def a(data):
     coords = [tuple(map(int, line.split(","))) for line in data.splitlines()]
@@ -28,37 +26,29 @@ def a(data):
 for example in puzzle.examples:
     if example.answer_a:
         example_answer = a(example.input_data)
-        print(f"Example answer: {example_answer} (expecting: {example.answer_a})")
+        print(f"Example answer: {example_answer} (expecting: {50})")
         assert str(example_answer) == "50"
 answer = a(puzzle.input_data)
 print("a:", answer)
-puzzle.answer_a = answer
+assert answer == 4769758290
 
 
 # Part b
 def b(data):
     red = [tuple(map(int, line.split(","))) for line in data.splitlines()]
     red.append(red[0])
-
-    polygon = Polygon(red)
-    #plot_polygon(polygon)
-    #plt.show()
-
-    def is_green(x, y):
-        return polygon.intersects(Point(x, y))
+    red_polygon = Polygon(red)
 
     m = 0
     for c0 in red:
         for c1 in red:
-            tmp = area(*c0, *c1)
-            if tmp <= m:
+            candidate_area = area(*c0, *c1)
+            if candidate_area <= m:
                 continue
-            #if not (is_green(c0[0], c1[1]) and is_green(c1[0], c0[1])):
-            #    continue
-            p2 = Polygon([c0, (c0[0], c1[1]), c1, (c1[0], c0[1]), c0])
-            if not polygon.contains(p2):
+            candidate_polygon = Polygon([c0, (c0[0], c1[1]), c1, (c1[0], c0[1]), c0])
+            if not red_polygon.contains(candidate_polygon):
                 continue
-            m = max(m, tmp)
+            m = max(m, candidate_area)
     return m
 
 
@@ -69,4 +59,4 @@ for example in puzzle.examples:
         assert str(example_answer) == "24"
 answer = b(puzzle.input_data)
 print("b:", answer)
-puzzle.answer_b = answer
+assert answer == 1588990708
