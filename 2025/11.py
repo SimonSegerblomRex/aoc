9d4f947387544f64021c2efc49f22a308b166538
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2025-12-11 06:20:47 +0100

    2025/11: Minor cleanup

--- 2025/11.py
+++ 2025/11.py
@@ -1,15 +1,11 @@
-import datetime
-import re
+from functools import cache
 
-import numpy as np
 from aocd.models import Puzzle
 
-YEAR = datetime.datetime.today().year
-DAY = datetime.datetime.today().day
+YEAR = 2025
+DAY = 11
 
 puzzle = Puzzle(year=YEAR, day=DAY)
-
-from functools import cache
 
 
 # Part a
@@ -18,35 +14,31 @@ def a(data):
     for line in data.splitlines():
         key, val = line.split(":")
         devices[key] = val.strip().split(" ")
+
     @cache
     def find_path(start, goal):
         if start == goal:
             return 1
         c = 0
         for device in devices[start]:
-            c +=find_path(device, goal)
+            c += find_path(device, goal)
         return c
+
     return find_path("you", "out")
-    breakpoint()
 
 
-for example in puzzle.examples:
-    if example.answer_a:
-        example_answer = a(example.input_data)
-        print(f"Example answer: {example_answer} (expecting: {example.answer_a})")
-        assert str(example_answer) == "5"
 answer = a(puzzle.input_data)
 print("a:", answer)
-puzzle.answer_a = answer
+assert answer == 506
 
 
 # Part b
 def b(data):
-    breakpoint()
     devices = {}
     for line in data.splitlines():
         key, val = line.split(":")
         devices[key] = val.strip().split(" ")
+
     @cache
     def find_path(start, goal, goal0=False, goal1=False):
         if start == goal:
@@ -55,27 +47,17 @@ def b(data):
             return 0
         c = 0
         for device in devices[start]:
-            c += find_path(device, goal, goal0=goal0 or device == "fft", goal1=goal1 or device == "dac")
+            c += find_path(
+                device,
+                goal,
+                goal0=goal0 or device == "fft",
+                goal1=goal1 or device == "dac",
+            )
         return c
+
     return find_path("svr", "out")
 
 
-example_answer = b("""svr: aaa bbb
-aaa: fft
-fft: ccc
-bbb: tty
-tty: ccc
-ccc: ddd eee
-ddd: hub
-hub: fff
-eee: dac
-dac: fff
-fff: ggg hhh
-ggg: out
-hhh: out
-""")
-print(f"Example answer: {example_answer} (expecting: 2)")
-assert str(example_answer) == "2"
 answer = b(puzzle.input_data)
 print("b:", answer)
-puzzle.answer_b = answer
+assert answer == 385912350172800
