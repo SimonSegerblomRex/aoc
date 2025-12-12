ebc9e848833d632b3f4c32c0373086269b369bbf
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2025-12-12 10:38:23 +0100

    2025/12: Minor cleanup

--- 2025/12.py
+++ 2025/12.py
@@ -13,28 +13,31 @@ def parse_shape(shape):
         for j, c in enumerate(line):
             if c == "#":
                 coords.add((j, i))
-    return (j + 1, i + 1, frozenset(coords))
+    return frozenset(coords)
 
 
 def a(data):
     *shapes, regions = data.split("\n\n")
     shapes = [parse_shape(shape) for shape in shapes]
-    shape_areas = [len(shape[2]) for shape in shapes]
+    shape_areas = [len(shape) for shape in shapes]
     s = 0
     for region in regions.splitlines():
         size, quantities = region.split(":")
-        width, height = list(map(int, size.split("x")))
-        quantities = list(map(int, quantities.strip().split()))
+        width, height = map(int, size.split("x"))
+        quantities = map(int, quantities.strip().split())
         rectangle_area = width * height
-        minimum_area_required = sum(shape_area * quantity for shape_area, quantity in zip(shape_areas, quantities))
-        if minimum_area_required < rectangle_area:
+        minimum_area_required = sum(
+            shape_area * quantity
+            for shape_area, quantity in zip(shape_areas, quantities)
+        )
+        if minimum_area_required <= rectangle_area:
             s += 1
     return s
 
 
 example_answer = a(puzzle.examples[0].input_data)
 print(f"Example answer: {example_answer} (expecting: {puzzle.examples[0].answer_a})")
-#assert str(example_answer) == example.answer_a
+# assert example_answer == int(puzzle.examples[0].answer_a)
 answer = a(puzzle.input_data)
 print("a:", answer)
 puzzle.answer_a = answer
