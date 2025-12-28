34d3562e37104112300fe154ba5ce721f9143f2e
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2025-12-28 03:18:29 +0100

    2025/10: Minor cleanup

--- 2025/10.py
+++ 2025/10.py
@@ -1,8 +1,3 @@
-import queue
-import re
-from collections import defaultdict
-from functools import cache
-
 import numpy as np
 from aocd.models import Puzzle
 from scipy.optimize import linprog
@@ -14,7 +9,6 @@ puzzle = Puzzle(year=YEAR, day=DAY)
 
 
 # Part a
-@cache
 def update_state(state, action):
     new_state = list(state)
     for idx in action:
@@ -22,54 +16,36 @@ def update_state(state, action):
     return tuple(new_state)
 
 
-def find_fewest_presses(start, goal, actions, b_check=False):
-    def h(next_state):
-        return 0
-
-    open_set = queue.PriorityQueue()
-    # (f_score, state, prev_action)
-    open_set.put((0, start, None))
-
-    g_score = defaultdict(lambda: 1 << 30)
-    g_score[(start, None)] = 0
-
-    f_score = defaultdict(lambda: 1 << 30)
-    f_score[(start, None)] = h(start)
-
-    while not open_set.empty():
-        _, curr_state, prev_action = open_set.get()
-        if curr_state == goal:
-            return int(g_score[(curr_state, prev_action)])
-
-        for action in actions:
-            if not b_check and action == prev_action:
-                continue
-            next_state = update_state(curr_state, action)
-            if b_check and any(s > g for s, g in zip(next_state, goal)):
-                continue
-            tentative_g_score = g_score[(curr_state, prev_action)] + 1
-            if tentative_g_score < g_score[(next_state, action)]:
-                g_score[(next_state, action)] = tentative_g_score
-                f_score[(next_state, action)] = tentative_g_score + h(next_state)
-                open_set.put(
-                    (
-                        f_score[(next_state, action)],
-                        next_state,
-                        action,
-                    )
-                )
+def find_fewest_presses(state, goal, actions):
+    states = [state]
+    visited = set()
+    presses = 0
+    while states:
+        new_states = []
+        presses += 1
+        for state in states:
+            for action in actions:
+                new_state = update_state(state, action)
+                if new_state == goal:
+                    return presses
+                if new_state in visited:
+                    continue
+                visited.add(new_state)
+                new_states.append(new_state)
+        states = new_states
 
 
 def a(data):
-    goals = re.findall(r"\[.+\]", data)
-    actions = re.findall(r"\](.+){", data)
-    all_goals = [tuple(c == "#" for c in goal.strip("[]")) for goal in goals]
-    all_actions = tuple(
-        tuple(tuple(map(int, e.strip("()").split(","))) for e in action.split())
-        for action in actions
-    )
+    goals = []
+    all_actions = []
+    for line in data.splitlines():
+        goal_txt, *actions, _ = line.split(" ")
+        goals.append(tuple(c == "#" for c in goal_txt.strip("[]")))
+        all_actions.append(
+            tuple(tuple(map(int, action.strip("()").split(","))) for action in actions)
+        )
     s = 0
-    for goal, actions in zip(all_goals, all_actions):
+    for goal, actions in zip(goals, all_actions):
         state = tuple([False] * len(goal))
         s += find_fewest_presses(state, goal, actions)
     return s
@@ -87,16 +63,16 @@ assert answer == 434
 
 # Part b
 def b(data):
-    goals = re.findall(r"{.+\}", data)
-    actions = re.findall(r"\](.+){", data)
-    all_goals = [tuple(map(int, goal.strip(r"{}").split(","))) for goal in goals]
-    all_actions = tuple(
-        tuple(tuple(map(int, e.strip("()").split(","))) for e in action.split())
-        for action in actions
-    )
-
+    goals = []
+    all_actions = []
+    for line in data.splitlines():
+        _, *actions, goal_txt = line.split(" ")
+        goals.append(tuple(map(int, goal_txt.strip(r"{}").split(","))))
+        all_actions.append(
+            tuple(tuple(map(int, action.strip("()").split(","))) for action in actions)
+        )
     s = 0
-    for ii, (goal, actions) in enumerate(zip(all_goals, all_actions)):
+    for ii, (goal, actions) in enumerate(zip(goals, all_actions)):
         A = np.zeros((len(goal), len(actions)), dtype=int)
         for j, action in enumerate(actions):
             for i in action:
